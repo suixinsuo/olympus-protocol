4c5e3b299ada4c40c0df1a57f37b8eb9e870a70a
Jerome Chen <jerome@aireach.io>
2018-03-13 12:37:11 +0800

    Added truffle projects and contracts

---
+++ contracts/libs/Provider.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.18;
+
+contract Provider {
+  enum ProvderType{
+      Strategy,
+      Pricing,
+      Exchange
+  }
+      
+  struct ProviderStatistic {
+      uint counter;
+  }    
+
+  struct ERC20Token {
+      string symbol;
+      address tokenAddress;
+      uint decimal;
+  }    
+
+  string public name;
+  ProvderType public providerType;
+  string public description;
+  mapping(string => bool) internal properties;
+  ProviderStatistic public statistics;
+}
