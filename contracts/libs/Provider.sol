d25f966d6448275ee87f32150ce354c84fd9cfd5
Jerome Chen <jerome@secondcompany.nl>
2018-03-21 17:23:24 +0800

    Provider contract update.

--- contracts/libs/Provider.sol
+++ contracts/libs/Provider.sol
@@ -2,25 +2,27 @@ pragma solidity ^0.4.18;
 
 
 contract Provider {
-  enum ProvderType {
-      Strategy,
-      Pricing,
-      Exchange
-  }
+    enum ProvderType {
+        Strategy,
+        Pricing,
+        Exchange
+    }
       
-  struct ProviderStatistic {
-      uint counter;
-  }    
+    struct ProviderStatistic {
+        uint counter;
+        uint amountInEther;
+        uint reputation;
+    }    
 
-  struct ERC20Token {
-      string symbol;
-      address tokenAddress;
-      uint decimal;
-  }
+    struct ERC20Token {
+        string symbol;
+        address tokenAddress;
+        uint decimal;
+    }
 
-  string public name;
-  ProvderType public providerType;
-  string public description;
-  mapping(string => bool) internal properties;
-  ProviderStatistic public statistics;
+    string public name;
+    ProvderType public providerType;
+    string public description;
+    mapping(string => bool) internal properties;
+    ProviderStatistic public statistics;
 }
