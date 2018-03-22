dbeb4a67f4b48fab066ef287ed52fe13fe0b7a1f
vic <vic_gao@olympuslabs.io>
2018-03-22 16:10:20 +0800

    update strategy

--- contracts/strategy/StrategyProviderInterface.sol
+++ contracts/strategy/StrategyProviderInterface.sol
@@ -5,37 +5,71 @@ import "../libs/Provider.sol";
 
 
 contract StrategyProviderInterface is Provider, Ownable {
+
+    struct Combo {
+        uint id;
+        string name;
+        string description;
+        string category;
+        bool isPrivate;      // false --> public/ true --> private
+        address[] tokenAddresses;
+        uint[] weights;      //total is 100
+        uint follower;
+        uint amount;
+    }
+
+    Combo[] public comboHub;
+
+    mapping(address => uint[]) public comboIndex;
+    mapping(uint => address) public comboOwner;
+
+    event ComboCreated(uint id, string name);
+    event ComboUpdated(uint id, string name);
+
+    modifier _checkIndex(uint _index) {
+        require(_index < comboHub.length);
+        _;
+    }
+
     event StrategyChanged(uint strategyId);
 
    // To core smart contract
-    function getStrategies() external returns (uint[] ids, string[] names, string[] descriptions);
+    function getStrategies(address _owner) public view returns (uint[] ids);
+    function getMyStrategies() public view returns (uint[] ids);
 
-    function getStrategy(uint strategyId) external returns (
+    function getStrategy(uint _index) public _checkIndex(_index)  view returns (
+        uint id, 
         string name, 
-        address owner, 
         string description, 
-        address[] tokens, 
-        uint[] weights);
+        string category,
+        address indexOwner, 
+        address[] tokenAddresses, 
+        uint[] weights, 
+        bool isPrivateIndex, 
+        uint follower,
+        uint amount);
 
    // To clients
     function createStrategy(
         string name, 
         string description, 
+        string category,
         address[] tokenAddresses, 
         uint[] weights, 
-        bool isPrivate, 
-        uint priceInMot) 
+        bool isPrivate) 
         public returns (uint strategyId);
 
     function updateStrategy(
         uint strategyId, 
-        string description, 
+        string name,
+        string description,
+        string category,
+        bool isPricate, 
         address[] tokenAddresses, 
-        uint[] weights, 
-        bool isPrivate, 
-        uint priceInMot) 
+        uint[] weights) 
         public returns (bool success);
 
     // increment statistics
-    // function incrementStatistics(uint id, uint amountInEther) external returns (bool success);        
+    //TODO atuh the core contract address
+    function incrementStatistics(uint id, uint amountInEther) external returns (bool success);        
 }
