625450af56768799b9420b3c7762d533a8848e88
Orange <orangemtony@gmail.com>
2018-03-23 15:30:59 +0800

    update
    update

--- contracts/price/PriceInterface.sol
+++ contracts/price/PriceInterface.sol
@@ -10,9 +10,9 @@ contract PriceProvider is Provider, Ownable {
     
     // To core smart contract
     function getSupportedTokens() external returns (address[] tokenAddresses);
-    function getPrices(address[] tokenAddresses) external returns (uint[] prices);
-    function getPrice(address tokenAddress) external returns (uint);
+    //function getPrices(address[] tokenAddresses) external returns (uint[] prices);
+    ffunction getPrice(address tokenAddresses) external returns (uint _prices);
     
     // TO Oracles. msg.sender is the address of that Oracle.
-    function updatePrices(address[] tokenAddresses, uint[] prices) external returns (bool success);
+    function updatePrices(address tokenAddresses, bytes32[] _Exchanges,  uint[] _prices, uint _nonce) external returns (bool success);
 }
