b10d6da093cfb09d590d605b820a3282e5773087
vic <vic_gao@olympuslabs.io>
2018-04-02 18:18:47 +0800

    mirror

--- contracts/strategy/StrategyProvider.sol
+++ contracts/strategy/StrategyProvider.sol
@@ -1,66 +1,123 @@
 pragma solidity ^0.4.18;
 
-// import "./StrategyProviderInterface.sol";
+import "./StrategyProviderInterface.sol";
+import "../libs/Converter.sol";
+
+contract StrategyProvider is StrategyProviderInterface {
+    event StrategyChanged(uint strategyId);
+
+    mapping(address => uint[]) public comboIndex;
+    mapping(uint => address) public comboOwner;
+
+    event ComboCreated(uint id, string name);
+    event ComboUpdated(uint id, string name);
 
 
-// contract StrategyProvider is StrategyProviderInterface {
-//     event StrategyChanged(uint strategyId);
+    function getStrategyCount() public view returns (uint length){
+        return comboHub.length;
+    }
 
-//     mapping(address => uint[]) public comboIndex;
-//     mapping(uint => address) public comboOwner;
 
-//     event ComboCreated(uint id, string name);
-//     event ComboUpdated(uint id, string name);
+    function getStrategies(address _owner) public view returns (uint[]) {
+        return comboIndex[_owner];
+    }
 
-//     function createStrategy(
-//         string _name,
-//         string _description,
-//         string _category,
-//         address[] _tokenAddresses,
-//         uint[] _weights,
-//         bool _isPrivate) 
-//         public returns(uint)
-//     {
 
-//         address owner = msg.sender;
-//         require(_checkCombo(_tokenAddresses, _weights));
-//         uint comboId = comboIndex[msg.sender].length;
-//         Combo memory myCombo = Combo(comboId, _name, _description, _category, _isPrivate, _tokenAddresses, _weights, 0, 0);
+    function getMyStrategies() public view returns (uint[]) {
+        return comboIndex[msg.sender];
+    }
 
-//         ComboCreated(myCombo.id, myCombo.name);
 
-//         uint index = comboHub.push(myCombo) - 1;
+    function getStrategyTokenCount(uint _index) public view returns (uint length){
+        return comboHub[_index].tokenAddresses.length;
+    }
 
-//         comboOwner[index] = owner;
-//         comboIndex[owner].push(index);
 
-//         return index;
-//     }
+    function getStrategyTokenByIndex(uint _index, uint tokenIndex) public view returns (address token, uint weight){
+        return (comboHub[_index].tokenAddresses[tokenIndex], comboHub[_index].weights[tokenIndex]);
+    }
 
-//     function updateStrategy(
-//         uint _index, 
-//         string _name, 
-//         string _description, 
-//         string _category,
-//         bool _isPrivate, 
-//         address[] _tokenAddresses, 
-//         uint[] _weights) 
-//         public returns (bool success) 
-//     {
+    function getStrategy(uint _index) public _checkIndex(_index) view returns (
+        uint id, 
+        bytes32 name, 
+        bytes32 description, 
+        bytes32 category,
+        address indexOwner, 
+        bool isPrivateIndex,
+        uint follower,
+        uint amount) 
+    {
+        Combo memory combo = comboHub[_index];
+        address owner = comboOwner[_index];
+        return (
+            combo.id,
+            Converter.stringToBytes32(combo.name),
+            Converter.stringToBytes32(combo.description),
+            Converter.stringToBytes32(combo.category),
+            owner, 
+            combo.isPrivate,
+            combo.follower,
+            combo.amount);
+    }
+    
 
-//         require(_checkCombo(_tokenAddresses, _weights));
-//         require(isOwner(_index));
+    function createStrategy(
+        string _name,
+        string _description,
+        string _category,
+        address[] _tokenAddresses,
+        uint[] _weights,
+        bool _isPrivate) 
+        public returns(uint)
+    {
 
-//         comboHub[_index].name = _name;
-//         comboHub[_index].description = _description;
-//         comboHub[_index].category = _category;
-//         comboHub[_index].isPrivate = _isPrivate;
-//         comboHub[_index].tokenAddresses = _tokenAddresses;
-//         comboHub[_index].weights = _weights;
+        address owner = msg.sender;
+        require(_checkCombo(_tokenAddresses, _weights));
+        uint comboId = comboIndex[msg.sender].length;
+        Combo memory myCombo = Combo(comboId, _name, _description, _category, _isPrivate, _tokenAddresses, _weights, 0, 0);
 
-//         ComboUpdated(comboHub[_index].id, comboHub[_index].name);
-//         return true;
-//     }
+        ComboCreated(myCombo.id, myCombo.name);
+
+        uint index = comboHub.push(myCombo) - 1;
+
+        comboOwner[index] = owner;
+        comboIndex[owner].push(index);
+
+        return index;
+    }
+
+
+    function updateStrategy(
+        uint _index, 
+        string _name, 
+        string _description, 
+        string _category,
+        bool _isPrivate, 
+        address[] _tokenAddresses, 
+        uint[] _weights) 
+        public returns (bool success) 
+    {
+
+        require(_checkCombo(_tokenAddresses, _weights));
+        // require(isOwner(_index));
+
+        comboHub[_index].name = _name;
+        comboHub[_index].description = _description;
+        comboHub[_index].category = _category;
+        comboHub[_index].isPrivate = _isPrivate;
+        comboHub[_index].tokenAddresses = _tokenAddresses;
+        comboHub[_index].weights = _weights;
+
+        ComboUpdated(comboHub[_index].id, comboHub[_index].name);
+        return true;
+    }
+
+    //TODO require core contract address
+    function incrementStatistics(uint _index, uint _amountInEther) external returns (bool){
+        comboHub[_index].amount += _amountInEther;
+    }  
+   // To clients
+
 
 //     function isPrivate(uint _index) public _checkIndex(_index) view returns(bool) {
 //         return comboHub[_index].isPrivate;
@@ -70,64 +127,14 @@ pragma solidity ^0.4.18;
 //         return comboOwner[_index] == msg.sender;
 //     }
 
-//     function getMyStrategies() public view returns (uint[]) {
-//         return comboIndex[msg.sender];
-//     }
 
-//     function getStrategies(address _owner) public view returns (uint[]) {
-//         return comboIndex[_owner];
-//     }
-
-//     function getStrategy(uint _index) public _checkIndex(_index) view returns (
-//         uint id, 
-//         string name, 
-//         string description, 
-//         string category,
-//         address indexOwner, 
-//         address[] tokenAddresses, 
-//         uint[] weights, 
-//         bool isPrivateIndex, 
-//         uint follower,
-//         uint amount) 
-//     {
-
-//         if ((isOwner(_index) || !isPrivate(_index))) {
-//             Combo storage combo = comboHub[_index];
-//             address owner = comboOwner[_index];
-//             return (
-//                 combo.id, 
-//                 combo.name, 
-//                 combo.description, 
-//                 combo.category,
-//                 owner, 
-//                 combo.tokenAddresses, 
-//                 combo.weights, 
-//                 combo.isPrivate, 
-//                 combo.follower,
-//                 combo.amount);
-//         } else {
-//             //TODO
-//             revert();
-//             // address[] memory tokenAddresses;
-//             // uint[] memory weights;
-//             // string memory name;
-//             // string memory description;
-//             // return (0, name,description, tokenAddresses[0], tokenAddresses, weights, false, 0);
-//         }
-//     }
-//     //TODO require core contract address
-//     function incrementStatistics(uint _index, uint _amountInEther) external returns (bool){
-//         comboHub[_index].amount += _amountInEther;
-//     }  
-
-//     function _checkCombo(address[] _tokenAddresses, uint[] _weights) internal pure returns(bool) {
-//         require(_tokenAddresses.length == _weights.length);
-//         uint total = 0;
-//         for (uint i = 0; i < _weights.length; ++i) {
-//             total += _weights[i];
-//         }
-//         return total == 100;
-//     }
-
+    function _checkCombo(address[] _tokenAddresses, uint[] _weights) internal pure returns(bool) {
+        require(_tokenAddresses.length == _weights.length);
+        uint total = 0;
+        for (uint i = 0; i < _weights.length; ++i) {
+            total += _weights[i];
+        }
+        return total == 100;
+    }
       
-// }
+}
--- truffle.js
+++ truffle.js
@@ -1,9 +1,28 @@
+'use strict'
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "hello world hello worl hello worl hello worl hello worl hello worl";
+var provider = new HDWalletProvider(mnemonic, "https://kovan.infura.io/Y1CDy31MpG0TX8eNqvfb");
+// console.log(provider.wallet["_privKey"].toString("hex"));
+
+//address: 0x0b23B055bC2e28BA0Bf91854585e8433755162a2
+//private key: a38e8e1e5f887b8bc0c31fb4f2b7f3eab9c08d7b360dec6a005149c5ff3bb3ee
+
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
-    }
+      network_id: '*' // Match any network id
+    },
+    mainnet: {
+      provider: provider,
+      network_id: '1'
+    },
+    kovan: {
+      provider: provider,
+      network_id: '42'
+    },
   }
 };
