7f0333c21a7545f1265de89c6ee2bd485d107364
Jerome Chen <jerome@aireach.io>
2018-04-03 18:03:26 +0800

    fixed errors

--- contracts/price/PriceProviderInterface.sol
+++ contracts/price/PriceProviderInterface.sol
@@ -1,14 +1,16 @@
 contract PriceOracleInterface {
 
-   function updatePrice(address _tokenAddress,bytes32[] _exchanges,uint[] _prices,uint _nonce) public returns(bool success);
+    function updatePrice(address _tokenAddress,bytes32[] _exchanges,uint[] _prices,uint _nonce) public returns(bool success);
 
-  function getNewDefaultPrice(address _tokenAddress) public view returns(uint);
+    function getNewDefaultPrice(address _tokenAddress) public view returns(uint);
 
-  function getNewCustomPrice(address _provider,address _tokenAddress) public view returns(uint);
+    function getNewCustomPrice(address _provider,address _tokenAddress) public view returns(uint);
 
-  function GetNonce(address providerAddress,address tokenAddress) public view returns(uint);
+    function GetNonce(address providerAddress,address tokenAddress) public view returns(uint);
 
-     function checkTokenSupported(address tokenAddress) view returns(bool success);
-     function checkExchangeSupported(bytes32 Exchanges) view returns(bool success);
-     function checkProviderSupported(address providerAddress,address tokenAddress) view returns(bool success);ion getToken(uint number) view returns(address);
+    function checkTokenSupported(address tokenAddress) public view returns(bool success);
+    function checkExchangeSupported(bytes32 Exchanges) public  view returns(bool success);
+    function checkProviderSupported(address providerAddress, address tokenAddress) public  view returns(bool success);
+    function getToken(uint number) public  view returns(address);
+}
 
