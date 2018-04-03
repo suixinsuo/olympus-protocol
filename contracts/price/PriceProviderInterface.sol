d970e7c6c61907d469546fe835079c41f7a9e3c8
Orange <orangemtony@gmail.com>
2018-04-03 18:03:11 +0800

    1
    1

--- contracts/price/PriceProviderInterface.sol
+++ contracts/price/PriceProviderInterface.sol
@@ -1,6 +1,6 @@
 contract PriceOracleInterface {
-
-   function updatePrice(address _tokenAddress,bytes32[] _exchanges,uint[] _prices,uint _nonce) public returns(bool success);
+    
+  function updatePrice(address _tokenAddress,bytes32[] _exchanges,uint[] _prices,uint _nonce) public returns(bool success);
 
   function getNewDefaultPrice(address _tokenAddress) public view returns(uint);
 
@@ -8,7 +8,20 @@ contract PriceOracleInterface {
 
   function GetNonce(address providerAddress,address tokenAddress) public view returns(uint);
 
+//   function getExchangeLength() view returns(uint);
+
+//   function getTokenLength() view returns(uint);
+
+//   function getProviderLength(address _tokenAddress) view returns(uint);
+
+//   function getExhchange(uint number) view returns(bytes32);
+
+//   function getProvider(uint number,address _tokenAddress) view returns(address);
+
+//   function getToken(uint number) view returns(address);
+  
      function checkTokenSupported(address tokenAddress) view returns(bool success);
      function checkExchangeSupported(bytes32 Exchanges) view returns(bool success);
-     function checkProviderSupported(address providerAddress,address tokenAddress) view returns(bool success);ion getToken(uint number) view returns(address);
-
+     function checkProviderSupported(address providerAddress,address tokenAddress) view returns(bool success);
+ 
+}
