5a9d0b5662278b4a92eba3be96e86c7e1e181a99
vic <vic_gao@olympuslabs.io>
2018-04-04 15:49:34 +0800

    mirror

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
 var Core = artifacts.require("./OlympusLabsCore.sol");
-<<<<<<< Updated upstream
-// var StrategyProvider = artifacts.require("./strategy/StrategyProvider");
-=======
 // var StrategyProvider = artifacts.require("./strategy/StrategyProvider.sol");
->>>>>>> Stashed changes
 // var ExchangeProvider = artifacts.require("./exchange/ExchangeProvider.sol");
 // var PriceProvider = artifacts.require("./price/PriceProvider.sol");
 
