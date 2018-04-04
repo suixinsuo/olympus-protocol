b9ae447e6c63cbc6532c6e20730b8923f8f68deb
vic <vic_gao@olympuslabs.io>
2018-04-04 15:40:10 +0800

    Merged develop.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var Core = artifacts.require("./OlympusLabsCore.sol");
+<<<<<<< Updated upstream
 // var StrategyProvider = artifacts.require("./strategy/StrategyProvider");
+=======
+// var StrategyProvider = artifacts.require("./strategy/StrategyProvider.sol");
+>>>>>>> Stashed changes
 // var ExchangeProvider = artifacts.require("./exchange/ExchangeProvider.sol");
 // var PriceProvider = artifacts.require("./price/PriceProvider.sol");
 
