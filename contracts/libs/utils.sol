7bfb07e4040e998870e46b574722b825ed1d096e
icattlecoder <icattlecoder@gmail.com>
2018-04-08 00:41:15 +0800

    exchange provider

---
+++ contracts/libs/utils.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+contract Utils {
+
+    uint  constant PRECISION = (10**18);
+    uint  constant MAX_DECIMALS = 18;
+
+    function calcDstQty(uint srcQty, uint srcDecimals, uint dstDecimals, uint rate) internal pure returns(uint) {
+        if( dstDecimals >= srcDecimals ) {
+            require((dstDecimals-srcDecimals) <= MAX_DECIMALS);
+            return (srcQty * rate * (10**(dstDecimals-srcDecimals))) / PRECISION;
+        } else {
+            require((srcDecimals-dstDecimals) <= MAX_DECIMALS);
+            return (srcQty * rate) / (PRECISION * (10**(srcDecimals-dstDecimals)));
+        }
+    }
+
+    function calcSrcQty(uint dstQty, uint srcDecimals, uint dstDecimals, uint rate) internal pure returns(uint) {
+        if( srcDecimals >= dstDecimals ) {
+            require((srcDecimals-dstDecimals) <= MAX_DECIMALS);
+            return (PRECISION * dstQty * (10**(srcDecimals - dstDecimals))) / rate;
+        } else {
+            require((dstDecimals-srcDecimals) <= MAX_DECIMALS);
+            return (PRECISION * dstQty) / (rate * (10**(dstDecimals - srcDecimals)));
+        }
+    }
+}
--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var prompt = require('prompt-sync')();
+
+var mnemonics = {};
+
 module.exports = {
   networks: {
     development: {
@@ -9,9 +14,17 @@ module.exports = {
   },
   mocha: {
     reporter: 'eth-gas-reporter',
-    reporterOptions : {
+    reporterOptions: {
       currency: 'CNY',
       gasPrice: 2
     }
+  },
+  kovan: {
+    provider: function () {
+      mnemonics.kovan = process.env.MNEMONICS || mnemonics.kovan || prompt('network kovan mnemonic: ');
+      return new HDWalletProvider(mnemonics.kovan, "https://kovan.infura.io/qajYHKaGssZt5WrdfzGP");
+    },
+    gasPrice: 1000000000,
+    network_id: 3
   }
 };
