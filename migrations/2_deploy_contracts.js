8bb5feb9b515c65e8b7b60fdaa2118269481f817
Gerdinand <gerdinand.hardeman@outlook.com>
2018-04-10 17:01:09 +0800

    Add tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,14 @@
 var Core = artifacts.require("./OlympusLabsCore.sol");
 var StrategyProvider = artifacts.require("./strategy/StrategyProvider.sol");
 //var ExchangeProvider = artifacts.require("./exchange/ExchangeProvider.sol");
- var PriceProvider = artifacts.require("./price/PriceProvider.sol");
-
+var PriceProvider = artifacts.require("./price/PriceProvider.sol");
+var ExtendedStorage = artifacts.require("./storage/OlympusStorageExtended.sol")
+var OlympusStorage = artifacts.require("./storage/OlympusStorage.sol");
 module.exports = function (deployer) {
   deployer.deploy(Core);
   deployer.deploy(StrategyProvider);
   //deployer.deploy(ExchangeProvider);
   deployer.deploy(PriceProvider);
+  deployer.deploy(ExtendedStorage);
+  deployer.deploy(OlympusStorage);
 }
