864831184532614068dfd6f5f4de247eafd54cb9
vic <vic_gao@olympuslabs.io>
2018-04-12 11:55:57 +0800

    update rbac

--- test/TestPermission.js
+++ test/TestPermission.js
@@ -45,4 +45,85 @@ contract('Olympus-Protocol-permission', (accounts) => {
    
         assert.equal(resHasRole.length, 0);
     })
+
+
+    it("should be able to create a strategy owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminAddStrategyOwner(accounts[1], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[1], "StrategyOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, true);
+    })
+
+    it("should be able to remove a strategy owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminRemoveStrategyOwner(accounts[1], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[1], "StrategyOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, false);
+    })
+
+    it("should be able to create a price owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminAddPriceOwner(accounts[2], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[2], "PriceOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, true);
+    })
+
+    it("should be able to remove a price owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminRemovePriceOwner(accounts[2], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[2], "PriceOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, false);
+    })
+
+    it("should be able to create a exchange owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminAddExchangeOwner(accounts[3], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[3], "ExchangeOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, true);
+    })
+
+    it("should be able to remove a exchange owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminRemoveExchangeOwner(accounts[3], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[3], "ExchangeOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, false);
+    })
+
+    it("should be able to create a storage owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminAddStorageOwner(accounts[4], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[4], "StorageOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, true);
+    })
+
+    it("should be able to remove a storage owner.", async () => {
+        let instance  = await Permission.deployed({from:accounts[0]});
+        let result = await instance.adminRemoveStorageOwner(accounts[4], {from:accounts[0]});
+
+        let resHasRole = await instance.hasRole(accounts[4], "StorageOwner");
+
+        assert.equal(result.receipt.status, 1); 
+        assert.equal(resHasRole, false);
+    })
 });
