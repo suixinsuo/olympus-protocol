b396f9c620fb1267dc7d7f11b0251ae8aacdf491
vic <vic_gao@olympuslabs.io>
2018-04-13 10:32:20 +0800

    update permission

--- contracts/rbac/PermissionProviderInterface.sol
+++ contracts/rbac/PermissionProviderInterface.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import "../libs/Provider.sol";
 import "zeppelin-solidity/contracts/ownership/rbac/RBAC.sol";
 
-contract PermissionProviderInterface is RBAC, Provider {
+contract PermissionProviderInterface is Provider, RBAC {
     
     modifier onlyAdmin()
     {
@@ -15,23 +15,23 @@ contract PermissionProviderInterface is RBAC, Provider {
     function adminAddRoleControl(address addr, string roleName) onlyAdmin public;
     function adminRemoveRoleControl(address addr, string roleName) onlyAdmin public;
 
-    // function adminAddCoreOwner(address addr) onlyAdmin public;
-    // function adminRemoveCoreOwner(address addr) onlyAdmin public;
-    // function checkCoreOwner(address addr) public view returns(bool success);
+    function adminAddCoreOwner(address addr) onlyAdmin public;
+    function adminRemoveCoreOwner(address addr) onlyAdmin public;
+    function hasCoreOwner(address addr) public view returns(bool success);
 
-    // function adminAddStrategyOwner(address addr) onlyAdmin public;
-    // function adminRemoveStrategyOwner(address addr) onlyAdmin public;
-    // function checkStrategyOwner(address addr) public view returns(bool success);
+    function adminAddStrategyOwner(address addr) onlyAdmin public;
+    function adminRemoveStrategyOwner(address addr) onlyAdmin public;
+    function hasStrategyOwner(address addr) public view returns(bool success);
     
-    // function adminAddPriceOwner(address addr) onlyAdmin public;
-    // function adminRemovePriceOwner(address addr) onlyAdmin public;
-    // function checkPriceOwner(address addr) public view returns(bool success);
+    function adminAddPriceOwner(address addr) onlyAdmin public;
+    function adminRemovePriceOwner(address addr) onlyAdmin public;
+    function hasPriceOwner(address addr) public view returns(bool success);
     
-    // function adminAddExchangeOwner(address addr) onlyAdmin public;
-    // function adminRemoveExchangeOwner(address addr) onlyAdmin public;
-    // function checkExchangeOwner(address addr) public view returns(bool success);
+    function adminAddExchangeOwner(address addr) onlyAdmin public;
+    function adminRemoveExchangeOwner(address addr) onlyAdmin public;
+    function hasExchangeOwner(address addr) public view returns(bool success);
     
-    // function adminAddStorageOwner(address addr) onlyAdmin public;
-    // function adminRemoveStorageOwner(address addr) onlyAdmin public;
-    // function checkStorageOwner(address addr) public view returns(bool success);
+    function adminAddStorageOwner(address addr) onlyAdmin public;
+    function adminRemoveStorageOwner(address addr) onlyAdmin public;
+    function hasStorageOwner(address addr) public view returns(bool success);
 }
