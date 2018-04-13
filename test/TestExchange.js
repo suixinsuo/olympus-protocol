bf50ea1a5ae4dccc89093fd8ee2064b03703dd92
icattlecoder <icattlecoder@gmail.com>
2018-04-13 21:08:16 +0800

    refect exchange adapter

--- test/TestExchange.js
+++ test/TestExchange.js
@@ -3,10 +3,15 @@ const KyberNetworkExchange = artifacts.require("../contracts/exchange/exchanges/
 const SimpleERC20Token = artifacts.require("../contracts/libs/SimpleERC20Token.sol");
 const ExchangeAdapterManager = artifacts.require("../contracts/exchange/ExchangeAdapterManager.sol");
 const ExchangeProvider = artifacts.require("../contracts/exchange/ExchangeProvider.sol");
+const ExchangeProviderWrap = artifacts.require("ExchangeProviderWrap");
 
 const tokenNum = 3;
 const ethToken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 const expectedRate = web3.toBigNumber('1000' + '000000000000000000');
+
+function bytes32ToString(bytes32){
+    return web3.toAscii(bytes32).replace(/\u0000/g,'');
+}
 
 contract('MockKyberNetwork', (accounts) => {
 
@@ -50,6 +55,24 @@ const OrderStatusErrored = 4;
 
 contract('KyberNetworkExchange', (accounts) => {
 
+    it("KyberNetworkExchange should be enable/disable.", async () => {
+        let mockKyber = await MockKyberNetwork.new(tokenNum);
+        let kyberExchange = await KyberNetworkExchange.new(mockKyber.address);
+
+        await kyberExchange.addExchange(0, "kyber");
+        // default is enabled
+        let enabled = await kyberExchange.isEnabled(0);
+        assert.ok(enabled);
+
+        await kyberExchange.disable(0)
+        enabled = await kyberExchange.isEnabled(0)
+        assert.ok(!enabled);
+
+        await kyberExchange.enable(0)
+        enabled = await kyberExchange.isEnabled(0)
+        assert.ok(enabled);
+    })
+
     it("KyberNetworkExchange should be able to placeOrder.", async () => {
 
         let mockKyber = await MockKyberNetwork.new(tokenNum);
@@ -70,13 +93,13 @@ contract('KyberNetworkExchange', (accounts) => {
         for (var i = 0; i < tokens.length; i++) {
 
             // Test getRate
-            let rate = await kyberExchange.getRate(tokens[i], 0);
+            let rate = await kyberExchange.getRate('', tokens[i], 0);
             assert.ok(expectedRate.equals(rate));
 
             let deposit = accounts[0];
             let srcAmountETH = 1;
             // Test placeOrder
-            let result = await kyberExchange.placeOrder(tokens[i], web3.toWei(srcAmountETH), rate, deposit);
+            let result = await kyberExchange.placeOrder('', tokens[i], web3.toWei(srcAmountETH), rate, deposit);
 
             let placedOrderEvent = result.logs.find(log => {
                 return log.event === 'PlacedOrder';
@@ -103,67 +126,68 @@ contract('KyberNetworkExchange', (accounts) => {
 })
 
 
+const ExchangeStatusEnabled = 0;
+const ExchangeStatusDisabled = 1;
+
 contract('ExchangeAdapterManager', (accounts) => {
 
-    it("ExchangeAdapterManager should be able to deploy and un/registerExchanger.", async () => {
+    it("ExchangeAdapterManager should be able to deploy and addExchange.", async () => {
 
-        // Test New without init adapter
-        let manager = await ExchangeAdapterManager.new(0);
-        assert.ok(manager)
-
+        let manager = await ExchangeAdapterManager.new();
         let mockKyber = await MockKyberNetwork.new(tokenNum);
         let kyberExchange = await KyberNetworkExchange.new(mockKyber.address);
 
-        // Test RegisterExchange
-        let result = await manager.registerExchange(kyberExchange.address);
-
-        let registedEvent = result.logs.find(log => {
-            return log.event === 'ExchangeAdapterRegisted';
+        let expectedExchangeName = "kyber";
+        // Test addExchange
+        let result = await manager.addExchange(expectedExchangeName,kyberExchange.address);
+        let addedEvent = result.logs.find(log => {
+            return log.event === 'AddedExchange';
         })
 
-        let actualRegistedAdapter = registedEvent.args.exchange;
-        let expectedRegistedAdapter = kyberExchange.address;
-        assert.equal(actualRegistedAdapter, expectedRegistedAdapter);
+        assert.ok(addedEvent, "expect emit AddedExchange event");
+        let exchangeId = addedEvent.args.id;
 
-        // Test Register an exsit exchange
-        result = await manager.registerExchange(kyberExchange.address);
+        // Test getExchangeInfo
+        result = await manager.getExchangeInfo(exchangeId);
+        let actualExchangeName = bytes32ToString(result[0]);
+        let actualExchangeStatus = result[1].toString();
+        assert.equal(actualExchangeName, expectedExchangeName, `expect exchange name is ${expectedExchangeName}, but got ${actualExchangeName}`);
+        assert.equal(actualExchangeStatus, ExchangeStatusEnabled+'', `expect exchange status is enabled(0), but got ${actualExchangeStatus}`);
 
-        assert.equal(result.logs.length, 0);
+        // Test getExchangeAdapter
+        let actualAdatperAddress = await manager.getExchangeAdapter(exchangeId)
+        assert.equal(actualAdatperAddress, kyberExchange.address);
 
-        // Test unregisterExchange
-        result = await manager.unregisterExchange(kyberExchange.address);
-        let unregistedEvent = result.logs.find(log => { return log.event === 'ExchangeAdapterUnRegisted'; })
-        assert.ok(unregistedEvent);
+        // Test getExchanges
+        let actualExchangesId = await manager.getExchanges()
+        assert.equal(actualExchangesId.length, 1);
+        assert.equal(actualExchangesId[0], exchangeId);
 
-        let actualUnRegistedAdapter = unregistedEvent.args.exchange;
-        let expectedUnRegistedAdapter = kyberExchange.address;
-        assert.equal(actualUnRegistedAdapter, expectedUnRegistedAdapter);
+        // disable Exchange
+        await kyberExchange.disable(exchangeId);
+        result = await manager.getExchangeInfo(exchangeId);
+        actualExchangeStatus = result[1].toString();
+        assert.equal(actualExchangeStatus, ExchangeStatusDisabled + '', `expect exchange status is diabled(1), but got ${actualExchangeStatus}`);
 
-        // Test UnRegister an unkown exchange
-        try {
-            result = await manager.unregisterExchange(kyberExchange.address);
-            assert.ok(false);
-        } catch (e) {
-            assert.ok(e);
-        }
-
-        // Test RegisterExchange Again
-        result = await manager.registerExchange(kyberExchange.address);
-        registedEvent = result.logs.find(log => { return log.event === 'ExchangeAdapterRegisted'; })
-        actualRegistedAdapter = registedEvent.args.exchange;
-        assert.equal(actualRegistedAdapter, expectedRegistedAdapter);
+        // enable Exchange
+        await kyberExchange.enable(exchangeId);
+        result = await manager.getExchangeInfo(exchangeId);
+        actualExchangeStatus = result[1].toString();
+        assert.equal(actualExchangeStatus, ExchangeStatusEnabled + '', `expect exchange status is enabled(0), but got ${actualExchangeStatus}`);
     })
 
     it("ExchangeAdapterManager checkTokenSupported.", async () => {
 
-        let manager = await ExchangeAdapterManager.new(0);
+        let manager = await ExchangeAdapterManager.new();
         let mockKyber = await MockKyberNetwork.new(tokenNum);
         let kyberExchange = await KyberNetworkExchange.new(mockKyber.address);
-        await manager.registerExchange(kyberExchange.address);
+        let expectedExchangeName = "kyber";
+        let result = await manager.addExchange(expectedExchangeName,kyberExchange.address);
+
         let tokens = await mockKyber.supportedTokens();
         for (var i = 0; i < tokens.length; i++) {
-            let isSupported = await manager.checkTokenSupported(tokens[i])
-            assert.ok(isSupported);
+            let isSupported = await manager.checkTokenSupported(tokens[i]);
+            assert.ok(isSupported, `expect token ${tokens[i]} is supported`);
         }
 
         let notSupported = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
@@ -173,23 +197,23 @@ contract('ExchangeAdapterManager', (accounts) => {
 
     it("ExchangeAdapterManager pickExchange.", async () => {
 
-        let manager = await ExchangeAdapterManager.new(0);
+        let manager = await ExchangeAdapterManager.new();
         let mockKyber = await MockKyberNetwork.new(tokenNum);
         let kyberExchange = await KyberNetworkExchange.new(mockKyber.address);
-        await manager.registerExchange(kyberExchange.address);
-        let tokens = await mockKyber.supportedTokens();
+        let expectedExchangeName = "kyber";
+        let result = await manager.addExchange(expectedExchangeName,kyberExchange.address);
+        let exchangeId = result.logs.find(log => { return log.event === 'AddedExchange'; }).args.id;
 
+        let tokens = await mockKyber.supportedTokens();
         for (var i = 0; i < tokens.length; i++) {
-            let exchange = await manager.pickExchange(tokens[i], 0, expectedRate)
-            assert.equal(exchange, kyberExchange.address);
+            let actualExchangeId = await manager.pickExchange(tokens[i], 0, expectedRate)
+            assert.ok(actualExchangeId, exchangeId, `expect token ${tokens[i]} is supported`);
         }
 
         let notSupported = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
-        let exchange = await manager.pickExchange(notSupported, 0, expectedRate)
-        assert.equal(exchange, '0x0000000000000000000000000000000000000000');
+        let actualExchangeId = await manager.pickExchange(notSupported, 0, expectedRate)
+        assert.equal(actualExchangeId, '0x0000000000000000000000000000000000000000000000000000000000000000');
     })
-
-
 })
 
 const MarketOrderStatusPending = 0;
@@ -203,11 +227,14 @@ contract('ExchangeProvider', (accounts) => {
 
     it("test placeOrder", async () => {
 
-        let manager = await ExchangeAdapterManager.new(0);
+        let manager = await ExchangeAdapterManager.new();
         let mockKyber = await MockKyberNetwork.new(tokenNum);
         let kyberExchange = await KyberNetworkExchange.new(mockKyber.address);
-        await manager.registerExchange(kyberExchange.address);
+        let expectedExchangeName = "kyber";
+        let result = await manager.addExchange(expectedExchangeName, kyberExchange.address);
+
         let exchangeProvider = await ExchangeProvider.new(manager.address);
+
         let tokens = await mockKyber.supportedTokens();
         let srcAmountETH = 1;
         let totalSrcAmountETH = srcAmountETH * tokens.length;
@@ -220,6 +247,7 @@ contract('ExchangeProvider', (accounts) => {
         for (let i = 0; i < tokens.length; i++) {
             let erc20Token = await SimpleERC20Token.at(tokens[i]);
             let actualBalance = await erc20Token.balanceOf(deposit);
+            assert.ok(actualBalance.equals(0));
         }
 
         await exchangeProvider.startPlaceOrder(orderId, deposit);
@@ -228,7 +256,7 @@ contract('ExchangeProvider', (accounts) => {
             await exchangeProvider.addPlaceOrderItem(orderId, tokens[i], web3.toWei(srcAmountETH), expectedRate);
         }
 
-        let result = await exchangeProvider.endPlaceOrder(orderId, { value: web3.toWei(totalSrcAmountETH) });
+        result = await exchangeProvider.endPlaceOrder(orderId, { value: web3.toWei(totalSrcAmountETH) });
 
         // Let's check those tokens's balance of deposit account
         for (let i = 0; i < tokens.length; i++) {
@@ -245,17 +273,60 @@ contract('ExchangeProvider', (accounts) => {
         }
     })
 
-    it("test checkToeknSupported", async () => {
+    it("test checkTokenSupported", async () => {
+
         let manager = await ExchangeAdapterManager.new(0);
         let mockKyber = await MockKyberNetwork.new(tokenNum);
         let kyberExchange = await KyberNetworkExchange.new(mockKyber.address);
-        await manager.registerExchange(kyberExchange.address);
+        let expectedExchangeName = "kyber";
+        let result = await manager.addExchange(expectedExchangeName, kyberExchange.address);
+
         let exchangeProvider = await ExchangeProvider.new(manager.address);
+
         let tokens = await mockKyber.supportedTokens();
 
         for (let i = 0; i < tokens.length; i++) {
             let isSupported = await exchangeProvider.checkTokenSupported(tokens[i]);
             assert.ok(isSupported);
+        }
+
+        let notSupported = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+        let isSupported = await exchangeProvider.checkTokenSupported(notSupported);
+        assert.ok(!isSupported);
+    })
+})
+
+contract('ExchangeProviderWrap', (accounts) => {
+
+
+    it("should be able to buy", async () => {
+        let manager = await ExchangeAdapterManager.new(0);
+        let mockKyber = await MockKyberNetwork.new(tokenNum);
+        let kyberExchange = await KyberNetworkExchange.new(mockKyber.address);
+        let expectedExchangeName = "kyber";
+        let result = await manager.addExchange(expectedExchangeName, kyberExchange.address);
+
+        let exchangeProvider = await ExchangeProvider.new(manager.address);
+        let exchangeProviderWrap = await ExchangeProviderWrap.new(exchangeProvider.address);
+
+        let tokens = await mockKyber.supportedTokens();
+        let srcAmountETH = 1;
+        let totalSrcAmountETH = srcAmountETH * tokens.length;
+
+        await kyberExchange.send(web3.toWei(totalSrcAmountETH, 'ether'));
+
+        let orderId = new Date().getTime();
+        let amounts = tokens.map(()=>{return web3.toWei(srcAmountETH,'ether')});
+        let rates = tokens.map(() => { return expectedRate });
+        let deposit = accounts[0];
+        result = await exchangeProviderWrap.buy(orderId, tokens, amounts, rates, deposit, { value: web3.toWei(totalSrcAmountETH, 'ether') });
+
+        // Let's check those tokens's balance of deposit account
+        for (let i = 0; i < tokens.length; i++) {
+            let erc20Token = await SimpleERC20Token.at(tokens[i]);
+            let actualBalance = await erc20Token.balanceOf(deposit);
+            let expectedBalance = expectedRate.mul(srcAmountETH);
+            assert.ok(expectedBalance.equals(actualBalance));
         }
     })
 })
