46f912828123ba2a2907a782f8cf31a5824adeb4
OrangeM21 <orangemtony@gmail.com>
2018-05-21 23:25:43 +0800

    fix

--- contracts/Tokenization/fundtemplate.sol
+++ contracts/Tokenization/fundtemplate.sol
@@ -1,5 +1,30 @@
 pragma solidity ^0.4.23;
 
+
+import "../permission/PermissionProviderInterface.sol";
+
 contract fundfundtemplate {
+    enum FUNDstatus { Pause, Close , Active }
+    
+    struct FUND {
+        uint id;
+        string name;
+        string description;
+        string category;
+        address[] tokenAddresses;
+        uint[] weights;
+        uint managementfee;
+        uint withdrawcycle; //*hours;
+        uint deposit;       //押金
+        bool riskcontrol;   //default true;
+        FUNDstatus status;
+        //uint follower; 放到另一个 struct 里面
+        //uint amount;   放到另一个 struct 里面
+        //bytes32 exchangeId;  不指定交易所
+    }
+    struct FUNDExtend {
+        address owner;
+    }
+    
     
 }
--- contracts/Tokenization/tokenization.sol
+++ contracts/Tokenization/tokenization.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.23;
 
 import "../permission/PermissionProviderInterface.sol";
-import "./TokenizationIndexInterface.sol";
-import "../lib/SafeMath.sol";
-import "./fundtemplate.sol"
+// import "./TokenizationIndexInterface.sol";
+import "../libs/SafeMath.sol";
+import "./fundtemplate.sol";
 
 
-contract TokenizationIndex is TokenizationIndexInterface  {
+contract TokenizationIndex {
     
     using SafeMath for uint256;
     
@@ -42,21 +42,36 @@ contract TokenizationIndex is TokenizationIndexInterface  {
         permissionProvider = PermissionProviderInterface(_permissionProvider);
     }
 
+    
+    //Create
     function CreatFUND(
         string _name,
         string _description,
         string _category,
-        address[] _tokenAddresses,
-        uint[] _weights
+        address[] memory _tokenAddresses,
+        uint[] memory _weights
     ) public 
     returns (address FundID) 
     {
         require(_checkLength(_tokenAddresses, _weights));
-        Fund  = new fundtemplate(_name,_description,_category,_tokenAddresses,_weights);
+        Fund = new fundtemplate(Fundlength,_name,  _description, _category, _tokenAddresses, _weights);
         FundOwner[Fundlength] = tx.origin;
         fundIndex[Fundlength] = Fund;
         Fundlength += 1;
         return Fund;
+    }
+
+
+    //Get
+    function getFUND(uint _FundIndex) public returns(
+        string _name,
+        string _description,
+        string _category,
+        address[] memory _tokenAddresses,
+        uint[] memory _weights
+    ){
+        require(_FundIndex<=Fundlength);
+
     }
 
     function _checkLength(address[] _tokenAddresses,uint[] _weights) internal returns(bool success){
