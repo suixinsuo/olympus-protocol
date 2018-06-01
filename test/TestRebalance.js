963ce13d4c1ec646f7187363a33c04af58867bac
Gerdinand <gerdinand.hardeman@outlook.com>
2018-06-01 17:33:18 +0800

    tests in progress

--- test/TestRebalance.js
+++ test/TestRebalance.js
@@ -19,21 +19,46 @@ contract('RebalanceMock', (accounts) => {
     try {
       let instance = await RebalanceMock.deployed();
       let tokenBalances = await instance.getTokenBalances.call();
-      console.log('initial tokenBalances', tokenBalances);
+      console.log('initial tokenBalances', tokenBalances.map((bigNo) => bigNo.toNumber()));
+      let initialTokenValues = await instance.getTokenValues.call();
+      console.log('initial tokenValues', initialTokenValues.map((bigNo) => bigNo.toNumber()));
+
+
+      console.log('total value', (await instance.getTotalIndexValue.call()).toNumber());
+
       let firstResult = await instance.rebalance.call();
       let firstTransactionResult = await instance.rebalance();
       assert.equal(firstResult, false);
       assert.equal(firstTransactionResult.receipt.status, TX_OK);
+      console.log('toSell', (await instance.getTokenLengths.call()).map((bigNo) => bigNo.toNumber()));
 
       let secondResult = await instance.rebalance.call();
       let secondTransactionResult = await instance.rebalance();
       assert.equal(secondResult, true);
       assert.equal(secondTransactionResult.receipt.status, TX_OK);
       let finalTokenBalances = await instance.getTokenBalances.call();
-      console.log('final tokenBalances', finalTokenBalances);
+      let finalTokenValues = await instance.getTokenValues.call();
+
+      console.log('total value', (await instance.getTotalIndexValue.call()).toNumber());
+      console.log('final tokenValues', finalTokenValues.map((bigNo) => bigNo.toNumber()));
+      console.log('final tokenBalances', finalTokenBalances.map((bigNo) => bigNo.toNumber()));
+      throw new Error();
     } catch (e) {
       console.error(e);
       throw e;
     }
   });
 });
+50000000000000000000
+2500000000000000000
+1250000000000000000
+5000000000000000000
+
+5000000000000000000
+2500000000000000000
+10000000000000000000
+
+10000000000000000000
+500000000000000000
+50000000000000000
+5000000000000000000
