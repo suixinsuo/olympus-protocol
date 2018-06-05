eb00cf3dc0686775d634a9782ccd10e1ce57dd05
Gerdinand <gerdinand.hardeman@outlook.com>
2018-06-05 17:26:06 +0800

    Start adding to index template

--- contracts/libs/IndexTemplate.sol
+++ contracts/libs/IndexTemplate.sol
@@ -3,11 +3,13 @@ import "../libs/SafeMath.sol";
 import "../permission/PermissionProviderInterface.sol";
 import "../price/PriceProviderInterface.sol";
 import "../libs/ERC20.sol";
+import "../libs/strings.sol";
+import "../libs/Converter.sol";
 import "../riskManagement/RiskManagementProviderInterface.sol";
 
 contract IndexTemplate {
+    using strings for *;
     using SafeMath for uint256;
-
     //Permission Control
     PermissionProviderInterface internal permissionProvider;
     //Price
@@ -33,9 +35,42 @@ contract IndexTemplate {
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
 
+    /**
+    Start Rebalance parameters
+     */
+    enum RebalanceStatus {
+        INACTIVE,
+        INITIATED,
+        READY_TO_TRADE,
+        SELLING_IN_PROGRESS,
+        SELLING_COMPLETE,
+        BUYING_IN_PROGRESS,
+        BUYING_COMPLETE
+    }
+    RebalanceStatus private rebalanceStatus = RebalanceStatus.INACTIVE;
+    uint private tokenStep = 10;
+    uint private rebalancingTokenProgress;
+    uint private PERCENTAGE_DENOMINATOR = 10000;
+    uint private rebalanceDeltaPercentage = 30; // 0.3%
+    uint private lastRebalance;
+    uint private rebalanceInterval = 0;
+    uint private ethValueRebalanceStart;
+    uint private rebalanceSoldTokensETHReceived;
+    struct RebalanceToken {
+        address tokenAddress;
+        uint tokenWeight;
+        uint amount;
+    }
+    RebalanceToken[] public rebalanceTokensToSell;
+    RebalanceToken[] public rebalanceTokensToBuy;
+    /**
+    End Rebalance parameters
+     */
+
     constructor (
         string _symbol, string _name, string _category, string _description,
-        uint _decimals, address[] _indexTokenAddresses, uint8[] _indexTokenWeights) public {
+        uint _decimals, address[] _indexTokenAddresses, uint8[] _indexTokenWeights,
+        uint _rebalanceInterval) public {
         require(_decimals <= 18, "Too many decimals, should be equal to or less than 18");
         decimals = _decimals;
         symbol = _symbol;
@@ -46,6 +81,8 @@ contract IndexTemplate {
         indexTokenAddresses = _indexTokenAddresses;
         indexTokenWeights = _indexTokenWeights;
         totalSupply = 0;
+        lastRebalance = now;
+        rebalanceInterval = _rebalanceInterval;
     }
 
     modifier onlyOwner(){
@@ -120,6 +157,149 @@ contract IndexTemplate {
     function setRiskProvider(address _riskProvider) public onlyOwner {
         riskProvider = RiskManagementProviderInterface(_riskProvider);
     }
+
+    /**
+    Start Rebalance Functions
+    */
+    function rebalancePrepareSellAndBuy() private returns (bool success){
+        if(rebalanceStatus == RebalanceStatus.INITIATED){
+            uint totalIndexValue = getTotalIndexValue();
+            uint i;
+            for(i = 0; i < tokenAddresses.length; i++) {
+                // Get the amount of tokens expected for 1 ETH
+                uint ETHTokenPrice = mockCoreGetPrice(ETH_TOKEN, tokenAddresses[i]);
+                if(ETHTokenPrice == 0){
+                    emit LogString("Error", "Price provider doesn't support this tokenIndex: ".toSlice().concat(Converter.bytes32ToString(bytes32(i)).toSlice()));
+                }
+                uint currentTokenBalance = ERC20(tokenAddresses[i]).balanceOf(address(this)); //
+                uint shouldHaveAmountOfTokensInETH = (totalIndexValue * tokenWeights[i]) / 100;
+                uint shouldHaveAmountOfTokens = (shouldHaveAmountOfTokensInETH * ETHTokenPrice) / 10**18;
+
+                // minus delta
+                if (shouldHaveAmountOfTokens < (currentTokenBalance - (currentTokenBalance * rebalanceDeltaPercentage / PERCENTAGE_DENOMINATOR))){
+                    rebalanceTokensToSell.push(RebalanceToken({
+                        tokenAddress: tokenAddresses[i],
+                        tokenWeight: tokenWeights[i],
+                        amount: currentTokenBalance - shouldHaveAmountOfTokens
+                    }));
+                // minus delta
+                } else if (shouldHaveAmountOfTokens > (currentTokenBalance + (currentTokenBalance * rebalanceDeltaPercentage / PERCENTAGE_DENOMINATOR))){
+                    rebalanceTokensToBuy.push(RebalanceToken({
+                        tokenAddress: tokenAddresses[i],
+                        tokenWeight: tokenWeights[i],
+                        // Convert token balance to ETH price (because we need to send ETH), taking into account the decimals of the token
+                        amount: ((shouldHaveAmountOfTokens - currentTokenBalance) * 10**18) / ETHTokenPrice
+                        // amount: shouldHaveAmountOfTokensInETH - (currentTokenBalance * (10**ERC20(tokenAddresses[i]).decimals()) / ETHTokenPrice)
+                    }));
+                }
+            //TODO Does this run out of gas for 100 tokens?
+            }
+            rebalanceStatus = RebalanceStatus.READY_TO_TRADE;
+        }
+        return true;
+    }
+
+    function rebalance() public returns (bool success){
+        // solium-disable-next-line security/no-block-members
+        require(lastRebalance + rebalanceInterval <= now, "Time is not here yet");
+        if(rebalanceStatus == RebalanceStatus.INACTIVE){
+            ethValueRebalanceStart = address(this).balance;
+            delete rebalanceTokensToSell;
+            delete rebalanceTokensToBuy;
+            rebalanceStatus = RebalanceStatus.INITIATED;
+        }
+        uint i;
+        uint currentProgress = rebalancingTokenProgress;
+        require(rebalancePrepareSellAndBuy(), "Prepare sell and buy failed");
+
+        if(rebalanceStatus == RebalanceStatus.READY_TO_TRADE || rebalanceStatus == RebalanceStatus.SELLING_IN_PROGRESS){
+            rebalanceStatus = RebalanceStatus.SELLING_IN_PROGRESS;
+            // First sell tokens
+            for(i = currentProgress; i < rebalanceTokensToSell.length; i++){
+                if(i > currentProgress + tokenStep){
+                    // Safety measure for gas
+                    // If the loop looped more than the tokenStep amount of times, we return false, and this function should be called again
+                    return false;
+                }
+                // TODO approve token transfers (depending on exchange implementation)
+                require(mockCoreExchange(rebalanceTokensToSell[i].tokenAddress,ETH_TOKEN,rebalanceTokensToSell[i].amount), "Exchange sale failed");
+                rebalancingTokenProgress++;
+                if(i == rebalanceTokensToSell.length - 1){
+                    rebalanceStatus = RebalanceStatus.SELLING_COMPLETE;
+                }
+            }
+        }
+
+
+        if(rebalanceStatus == RebalanceStatus.SELLING_COMPLETE){
+            rebalanceSoldTokensETHReceived = address(this).balance - ethValueRebalanceStart;
+            rebalanceStatus = RebalanceStatus.BUYING_IN_PROGRESS;
+        }
+
+        // Then buy tokens
+        if(rebalanceStatus == RebalanceStatus.BUYING_IN_PROGRESS){
+            uint sellTxs = rebalancingTokenProgress - currentProgress;
+            rebalancingTokenProgress = 0;
+            uint assumedAmountOfEthToBuy;
+            uint differencePercentage;
+            bool surplus;
+
+            // Get the total amount of ETH that we are supposed to buy
+            for(i = 0; i < rebalanceTokensToBuy.length; i++){
+                assumedAmountOfEthToBuy += rebalanceTokensToBuy[i].amount;
+            }
+
+            // Based on the actual amount of received ETH for sold tokens, calculate the difference percentage
+            // So this can be used to modify the ETH used, so we don't have an ETH shortage or leftovers at the last token buy
+            if(assumedAmountOfEthToBuy > rebalanceSoldTokensETHReceived){
+                differencePercentage = ((assumedAmountOfEthToBuy - rebalanceSoldTokensETHReceived) * PERCENTAGE_DENOMINATOR) / assumedAmountOfEthToBuy;
+            } else if (assumedAmountOfEthToBuy < rebalanceSoldTokensETHReceived){
+                surplus = true;
+                differencePercentage = ((rebalanceSoldTokensETHReceived - assumedAmountOfEthToBuy) * PERCENTAGE_DENOMINATOR) / rebalanceSoldTokensETHReceived;
+            } else {
+                differencePercentage = 0;
+            }
+
+            for(i = rebalancingTokenProgress; i < rebalanceTokensToBuy.length; i++){
+
+                if(i + sellTxs > currentProgress + tokenStep){
+                    // Safety measure for gas
+                    // If the loop looped more than the tokenStep amount of times, we return false, and this function should be called again
+                    // Also take into account the number of sellTxs that have happened in the current function call
+                    return false;
+                }
+                uint slippage;
+
+                if(differencePercentage > 0){
+                    // Calculate the actual amount we should buy, based on the actual ETH received from selling tokens
+                    slippage = (rebalanceTokensToBuy[i].amount * differencePercentage) / PERCENTAGE_DENOMINATOR;
+                }
+                if(surplus == true){
+                    require(mockCoreExchange(ETH_TOKEN,rebalanceTokensToBuy[i].tokenAddress,rebalanceTokensToBuy[i].amount + slippage), "Exchange buy failed");
+                } else {
+                    require(mockCoreExchange(ETH_TOKEN,rebalanceTokensToBuy[i].tokenAddress,rebalanceTokensToBuy[i].amount - slippage), "Exchange buy failed");
+                }
+                rebalancingTokenProgress++;
+                if(i == rebalanceTokensToBuy.length - 1){
+                    rebalanceStatus = RebalanceStatus.BUYING_COMPLETE;
+                }
+            }
+
+        }
+        if(rebalanceStatus == RebalanceStatus.BUYING_COMPLETE){
+            // Yay, done! Reset everything, ready for the next time
+            // solium-disable-next-line security/no-block-members
+            lastRebalance = now;
+            rebalanceStatus = RebalanceStatus.INACTIVE;
+            rebalancingTokenProgress = 0;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+    End Rebalance Functions
+    */
 
 	  // Event which is triggered to log all transfers to this contract's event log
     event Transfer(
