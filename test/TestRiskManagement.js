8faf5218bf31528794227f314f43fe379fbf3a7e
Abel Bordonado Lillo <abel.bordonado@gmail.com>
2018-06-05 12:04:20 +0800

    Add risk managment to Index buy and seel token

--- test/TestRiskManagement.js
+++ test/TestRiskManagement.js
@@ -22,20 +22,20 @@ contract.only("TestRiskManagement", (accounts) => {
   })
 
   it.only('The transaction has risk', async () => {
-    const hasRisk = await riskProvider.hasRisk(accounts[0], accounts[1], token, 1);
+    const hasRisk = await riskProvider.hasRisk(accounts[0], accounts[1], token, 1, 1);
     // TODO Check  situation without risk
     assert.equal(false, hasRisk);
   })
 
   it.only('The transaction has no risk', async () => {
-    const hasRisk = await riskProvider.hasRisk(accounts[0], accounts[1], token, 1);
+    const hasRisk = await riskProvider.hasRisk(accounts[0], accounts[1], token, 1, 1);
     // TODO Check  situation with risk
     assert.equal(true, true || hasRisk); // TODO remove the 'true ||' now is always returning false
   })
 
   it.only('Risk manager is disabled', async () => {
     // TODO Check real situation with risk
-    const hasRisk = await riskProvider.hasRisk(accounts[0], accounts[1], token, 1);
+    const hasRisk = await riskProvider.hasRisk(accounts[0], accounts[1], token, 1, 1);
     await riskProvider.disable();
 
     assert.equal(false, hasRisk);
