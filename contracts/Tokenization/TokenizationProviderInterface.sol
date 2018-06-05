294fb78ba15e03eb3b7fbe4b4cdef1103ce73c62
OrangeM21 <orangemtony@gmail.com>
2018-06-05 13:07:36 +0800

    fix

--- contracts/Tokenization/TokenizationProviderInterface.sol
+++ contracts/Tokenization/TokenizationProviderInterface.sol
@@ -1,7 +1,31 @@
 pragma solidity ^0.4.23;
 
 contract TokenizationProviderInterface {
+    function createFund(
+        string _name,
+        string _symbol,
+        uint _decimals,
+        string _description,
+        string _category,
+        address[] memory _tokenAddresses,
+        uint[] memory _weights,
+        uint _withdrawCycle,
+        uint _lockTime
+    ) public 
+    ///////WARNING 
+    //onlyWhitelist
+    returns(address FundAddress);
 
-
-    
+    function getFundDetails(uint _fundId) public view returns(
+        address _owner,
+        string _name,
+        string _symbol,
+        uint _totalSupply,
+        string _description,
+        string _category,
+        address[]  _tokenAddresses,
+        uint[]  _weights
+    );
+    function getFundOwner(uint _fundId) public view returns(address _fundOwner);
+    function getFundAddress(uint _fundId) public view returns(address _fundAddress);
 }
