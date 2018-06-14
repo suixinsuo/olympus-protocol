d654d415c592ad09c5b4e79f531c4d8ff6f59907
Jerome Chen <jerome@secondcompany.nl>
2018-06-14 18:01:31 +0800

    Removed fund/index templates.

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.4.24;
 
 import "./interfaces/MarketplaceInterface.sol";
+import "./interfaces/DerivativeInterface.sol";
 
 
 contract Marketplace is MarketplaceInterface {
@@ -8,9 +9,9 @@ contract Marketplace is MarketplaceInterface {
     mapping(address => address[]) public productMappings;
 
     function registerProduct() external returns(bool success) {
+        DerivativeInterface derivative = DerivativeInterface(msg.sender);
         products.push(msg.sender);
-        //solhint-disable-next-line
-        address creator = tx.origin;
+        address creator = derivative.owner();
         for (uint i = 0; i < productMappings[creator].length; i++) {
             require(productMappings[creator][i] != msg.sender);
         }
---
+++ contracts/MockIndex.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.4.24;
+
+import "./Derivative.sol";
+import "./interfaces/IndexInterface.sol";
+
+
+contract MockIndex is Derivative, IndexInterface {
+    //
+}
