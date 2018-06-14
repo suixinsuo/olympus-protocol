7afdae97399c58c4828132d061967e501c6918db
Jerome Chen <jerome@secondcompany.nl>
2018-06-14 17:19:06 +0800

    Changed marketplace interface.

--- contracts/interfaces/MarketplaceInterface.sol
+++ contracts/interfaces/MarketplaceInterface.sol
@@ -1,10 +1,14 @@
 pragma solidity 0.4.24;
 
+import "../libs/Ownable.sol";
 
-contract MarketplaceInterface {
+
+contract MarketplaceInterface is Ownable {
 
     address[] public products;
+    mapping(address => address[]) public productMappings;
 
     function registerProduct() external returns(bool success);
+    function unregisterProduct() external returns(bool success);
     function getOwnProducts() external returns (address[] addresses);
 }
