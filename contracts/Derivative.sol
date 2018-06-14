b07b524fcc34660d5afe45d18368ec8ebc82b8f1
Jerome Chen <jerome@aireach.io>
2018-06-14 17:19:06 +0800

    Changed marketplace interface.

---
+++ contracts/Derivative.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.4.24;
+
+import "./interfaces/DerivativeInterface.sol";
+
+
+contract Derivative is DerivativeInterface {
+    
+}
---
+++ contracts/MockFund.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.4.24;
+
+import "./Derivative.sol";
+
+
+contract Derivative is Derivative {
+    
+}
