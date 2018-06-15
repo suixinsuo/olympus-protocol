26d7306dcd361cdd449b2e9e76aad96bf3677b12
Gerdinand <gerdinand.hardeman@outlook.com>
2018-06-15 12:30:43 +0800

    create price provider and exchange interfaces

--- contracts/interfaces/PriceProviderInterface.sol
+++ contracts/interfaces/PriceProviderInterface.sol
@@ -5,6 +5,15 @@ import "./ComponentInterface.sol";
 
 
 contract PriceProviderInterface is ComponentInterface {
-    function getPrice(string _exchangeId, address _sourceAddress, address _destAddress, uint _amount) 
+    /*
+     * @dev Returns the expected price for 1 of sourceAddress.
+     * For ETH, use 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+     * @param address _sourceAddress The token to sell for the destAddress.
+     * @param address _destAddress The token to buy with the source token.
+     * @param uint _amount The amount of tokens which is wanted to buy.
+     * @param bytes32 _exchangeId The exchangeId to choose. If it's an empty string, then the exchange will be chosen automatically.
+     * @return returns the expected and slippage rate for the specified conversion
+     */
+    function getPrice(address _sourceAddress, address _destAddress, uint _amount, bytes32 _exchangeId)
         external returns(uint expectedRate, uint slippageRate);
 }
---
+++ contracts/interfaces/implementations/OlympusExchangeInterface.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.4.24;
+
+import "../ExchangeInterface.sol";
+import "../PriceProviderInterface.sol";
+
+contract OlympusExchangeInterface is ExchangeInterface, PriceProviderInterface {
+    /*
+     * @dev Buy multiple tokens at once with ETH.
+     * @param ERC20[] _tokens The tokens to buy, should be an array of ERC20 addresses.
+     * @param uint[] _amounts Amount of ETH used to buy this token. Make sure the value sent to this function is the same as the sum of this array.
+     * @param uint[] _minimumRates The minimum amount of tokens to receive for 1 ETH.
+     * @param address _depositAddress The address to send the bought tokens to.
+     * @param bytes32 _exchangeId The exchangeId to choose. If it's an empty string, then the exchange will be chosen automatically.
+     * @param address _partnerId If the exchange supports a partnerId, you can supply your partnerId here
+     * @return boolean boolean whether or not the trade succeeded.
+     */
+    function buyTokens
+        (
+        ERC20[] _tokens, uint[] _amounts, uint[] _minimumRates,
+        address _depositAddress, bytes32 _exchangeId, address _partnerId
+        ) external payable returns(bool success);
+
+    /*
+     * @dev Sell multiple tokens at once with ETH, make sure all of the tokens are approved to be transferred beforehand with the Olympus Exchange address.
+     * @param ERC20[] _tokens The tokens to sell, should be an array of ERC20 addresses.
+     * @param uint[] _amounts Amount of tokens to sell this token. Make sure the value sent to this function is the same as the sum of this array.
+     * @param uint[] _minimumRates The minimum amount of ETH to receive for 1 specified ERC20 token.
+     * @param address _depositAddress The address to send the bought tokens to.
+     * @param bytes32 _exchangeId The exchangeId to choose. If it's an empty string, then the exchange will be chosen automatically.
+     * @param address _partnerId If the exchange supports a partnerId, you can supply your partnerId here
+     * @return boolean boolean whether or not the trade succeeded.
+     */
+    function sellTokens
+        (
+        ERC20[] _tokens, uint[] _amounts, uint[] _rates,
+        address _depositAddress, string _exchangeId, address _partnerId
+        ) external returns(bool success);
+}
