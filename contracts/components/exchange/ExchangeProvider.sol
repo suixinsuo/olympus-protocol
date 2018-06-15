33b38c2b154c3be27f8aadf067d8fae4d1d1c885
Gerdinand <gerdinand.hardeman@outlook.com>
2018-06-15 19:43:03 +0800

    add buy and sell single token

--- contracts/components/exchange/ExchangeProvider.sol
+++ contracts/components/exchange/ExchangeProvider.sol
@@ -14,6 +14,53 @@ contract ExchangeProvider is OlympusExchangeInterface {
         exchangeManager = OlympusExchangeAdapterManagerInterface(_exchangeManager);
     }
 
+    function buyToken
+        (
+        ERC20 _token, uint _amount, uint _minimumRate,
+        address _depositAddress, bytes32 _exchangeId, address _partnerId
+        ) external payable returns(bool success) {
+        require(msg.value == _amount);
+        OlympusExchangeAdapterInterface adapter;
+        bytes32 exchangeId = _exchangeId == "" ? exchangeManager.pickExchange(_token, _amount, _minimumRate) : _exchangeId;
+        if(exchangeId == 0){
+            return false;
+        }
+        adapter = OlympusExchangeAdapterInterface(exchangeManager.getExchangeAdapter(exchangeId));
+        require(
+            adapter.buyToken.value(msg.value)(
+                _token,
+                _amount,
+                _minimumRate,
+                _depositAddress,
+                "",
+                _partnerId)
+        );
+        return true;
+    }
+
+    function sellToken
+        (
+        ERC20 _token, uint _amount, uint _minimumRate,
+        address _depositAddress, bytes32 _exchangeId, address _partnerId
+        ) external returns(bool success) {
+
+        OlympusExchangeAdapterInterface adapter;
+        bytes32 exchangeId = _exchangeId == "" ? exchangeManager.pickExchange(_token, _amount, _minimumRate) : _exchangeId;
+        if(exchangeId == 0){
+            return false;
+        }
+        adapter = OlympusExchangeAdapterInterface(exchangeManager.getExchangeAdapter(exchangeId));
+        require(
+            adapter.sellToken(
+                _token,
+                _amount,
+                _minimumRate,
+                _depositAddress,
+                _partnerId)
+            );
+        return true;
+    }
+
     function buyTokens(ERC20[] tokens, uint256[] amounts, uint256[] rates, bytes32 _exchangeId, address deposit) external payable returns(bool) {
         OlympusExchangeAdapterInterface adapter;
         for (uint i = 0; i < tokens.length; i++ ) {
