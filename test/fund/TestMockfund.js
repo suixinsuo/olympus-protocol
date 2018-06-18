a73b7bda2b8dbd013328f3b59aab56eef020e260
Abel Bordonado Lillo <abel.bordonado@gmail.com>
2018-06-18 15:35:41 +0800

    Added some information and fixes after kovan

--- test/fund/TestMockfund.js
+++ test/fund/TestMockfund.js
@@ -1,6 +1,8 @@
 const log = require('../utils/log');
 const FundInstance = artifacts.require("../../contracts/components/mocks/MockFund.sol");
 
+const TOKEN1 = 0x0041dee9f481a1d2aa74a3f1d0958c1db6107c686a;
+const TOKEN2 = 0x3fb1c5555a04fc478784846296a35d1d2bf7e57c;
 
 contract('Mock Fund', (accounts) => {
   let fund;
@@ -11,13 +13,24 @@ contract('Mock Fund', (accounts) => {
     fund = await FundInstance.deployed();
   });
 
-  it("Fund shall deploy", async () => log.catch(async () => {
-    await fund.sendTransaction({ value: web3.toWei(1, 'ether'), from: investorA });
-    await fund.sendTransaction({ value: web3.toWei(1, 'ether'), from: investorB });
+  it("Fund shall be able to buy and sell tokens", async () => log.catch(async () => {
+
+    await fund.invest({ value: web3.toWei(1, 'ether'), from: investorA });
+    await fund.invest({ value: web3.toWei(1, 'ether'), from: investorB });
 
     assert.equal((await fund.totalSupply()).toNumber(), web3.toWei(2, 'ether'));
 
-    // fund.buyTokens('',)
+    // fund.buyTokens('', [TOKEN1, TOKEN2], [web3.toWei(1, 'ether'), web3.toWei(1, 'ether')], [1, 1]);
+    // const tokens = await fund.getTokens();
+    // tokens[1] = tokens[1].map((amount) => amount.toNumber());
+
+    // TODO assert when exchange provider
+
+    // fund.sellTokens('', [TOKEN1, TOKEN2], [web3.toWei(1, 'ether'), web3.toWei(1, 'ether')], [1, 1]);
+    // const tokensAfter = await fund.getTokens();
+    // tokensAfter[1] = tokensAfter[1].map((amount) => amount.toNumber());
+
+    // TODO assert when exchange provider
   }));
 
 
