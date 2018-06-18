2a6459926bf951dff3aad57279a62599c6a5c381
Abel Bordonado Lillo <abel.bordonado@gmail.com>
2018-06-18 11:14:05 +0800

    Merge with Withdraw Branch

--- contracts/components/mocks/MockFund.sol
+++ contracts/components/mocks/MockFund.sol
@@ -6,4 +6,9 @@ import "../../interfaces/FundInterface.sol";
 
 contract MockFund is FundInterface, MockDerivative {
     uint public todo;
+    function buyTokens(string _exchangeId, ERC20[] _tokens, uint[] _amounts, uint[] _rates)
+        public returns(bool success);
+
+    function sellTokens(string _exchangeId, ERC20[] _tokens, uint[] _amounts, uint[] _rates)
+        public returns(bool success);
 }
