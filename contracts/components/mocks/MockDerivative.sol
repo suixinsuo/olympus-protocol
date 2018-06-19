65ddbf4889570c3163dbc0a68aeb1c8a7774d038
Abel Bordonado Lillo <abel.bordonado@gmail.com>
2018-06-19 16:04:46 +0800

    Inehritance from Derivative only

--- contracts/components/mocks/MockDerivative.sol
+++ contracts/components/mocks/MockDerivative.sol
@@ -1,10 +1,10 @@
 pragma solidity 0.4.24;
 
-import "../../interfaces/DerivativeInterface.sol";
+import "../../Derivative.sol";
 import "../../components/base/ComponentContainer.sol";
 import "../../interfaces/MarketplaceInterface.sol";
 
-contract MockDerivative is  DerivativeInterface, ComponentContainer  {
+contract MockDerivative is  Derivative {
 
     string public constant MARKET = "Marketplace";
     uint256 public totalSupply = 0;
@@ -17,49 +17,6 @@ contract MockDerivative is  DerivativeInterface, ComponentContainer  {
     function invest() public payable returns(bool success) {return true;}
     function changeStatus(DerivativeStatus) public returns(bool) {return true;}
     function getPrice() public view returns(uint)  { return 10**decimals;}
-    // ----------- ERC20 ----------
-    mapping(address => uint256) balances;
-    mapping(address => mapping (address => uint256)) allowed;
-
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-
-    function totalSupply() public view returns (uint supply) {
-        return balances[address(this)];
-    }
-
-    function decimals() public view returns (uint) {
-        return decimals;
-    }
-
-    function balanceOf(address tokenOwner) public view returns (uint balance) {
-        return balances[tokenOwner];
-    }
-
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = balances[msg.sender] - tokens;
-        balances[to] = balances[to] + tokens;
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = balances[from] - tokens;
-        allowed[from][msg.sender] = allowed[from][msg.sender] - tokens;
-        balances[to] = balances[to] + tokens;
-        emit Transfer(from, to, tokens);
-        return true;
-    }
-
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
 
 }
 
