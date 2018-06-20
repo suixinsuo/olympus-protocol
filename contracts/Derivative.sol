75792e2890368847052e38475ddc1d58473b46d7
Abel Bordonado Lillo <abel.bordonado@gmail.com>
2018-06-20 13:51:08 +0800

    Changes as per discussion

--- contracts/Derivative.sol
+++ contracts/Derivative.sol
@@ -8,5 +8,9 @@ import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 // Abstract class that implements the common functions to all our derivatives
 contract Derivative is DerivativeInterface, ComponentContainer, StandardToken {
 
+    function setProvider(string name, address provider) external onlyOwner returns(bool) {
+        setComponent(name, provider);
+        return true;
+    }
 
 }
