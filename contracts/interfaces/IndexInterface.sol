fd6dc24eb10ee40535a82a35da226776ceb0e836
Gerdinand <gerdinand.hardeman@outlook.com>
2018-06-21 18:38:30 +0800

    untested rebalance

--- contracts/interfaces/IndexInterface.sol
+++ contracts/interfaces/IndexInterface.sol
@@ -9,4 +9,6 @@ contract IndexInterface is DerivativeInterface {
 
     // this should be called until it returns true.
     function rebalance() public returns (bool success);
+
+    function getTokens() public view returns (address[] _tokens, uint[] _weights); // TODO remove before merge
 }
