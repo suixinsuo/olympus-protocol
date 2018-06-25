013fda6fbcea1215dea90daf5e8b7b37c09b5562
yaohao9 <986403268@qq.com>
2018-06-25 19:34:13 +0800

    push

---
+++ contracts/ComponentList.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.4.24;
+
+import "./interfaces/ComponentInterface.sol";
+
+contract MockComponentList {
+
+    mapping(string => mapping(string => address)) components;
+
+    event ComponentUpdated (string _name, string _version, address _componentAddress);
+
+    function setComponent(string _name, address _componentAddress) public payable {
+        ComponentInterface c = ComponentInterface(_componentAddress);
+        components[_name][c.version()] = _componentAddress;
+        emit ComponentUpdated(_name, c.version(), _componentAddress);
+    }
+
+    function getComponent(string _name, string _version) public view returns (address){
+        return components[_name][_version];
+    }
+}
