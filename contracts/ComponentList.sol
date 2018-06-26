7ada1f664e4cedf69c79448bfbf539bd37b6de18
yaohao9 <986403268@qq.com>
2018-06-26 10:57:00 +0800

    Ownable support

--- contracts/ComponentList.sol
+++ contracts/ComponentList.sol
@@ -1,14 +1,15 @@
 pragma solidity 0.4.24;
 
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./interfaces/ComponentInterface.sol";
 
-contract ComponentList {
+contract ComponentList is Ownable{
 
     mapping(string => mapping(string => address)) components;
 
     event ComponentUpdated (string _name, string _version, address _componentAddress);
 
-    function setComponent(string _name, address _componentAddress) public payable {
+    function setComponent(string _name, address _componentAddress) public payable onlyOwner{
         ComponentInterface c = ComponentInterface(_componentAddress);
         components[_name][c.version()] = _componentAddress;
         emit ComponentUpdated(_name, c.version(), _componentAddress);
@@ -17,4 +18,5 @@ contract ComponentList {
     function getComponent(string _name, string _version) public view returns (address){
         return components[_name][_version];
     }
+    
 }
