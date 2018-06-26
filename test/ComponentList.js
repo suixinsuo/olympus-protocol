812f292796805b92d5d4e32af1b7eaf82b587967
yaohao9 <986403268@qq.com>
2018-06-26 10:45:56 +0800

    adjust the component list code

--- test/ComponentList.js
+++ test/ComponentList.js
@@ -1,5 +1,5 @@
 const log = require('./utils/log');
-const ComponentList = artifacts.require("../contracts/ComponentList.sol");
+const ComponentList = artifacts.require("../contracts/ComponentList");
 const Reimbursable = artifacts.require("../contracts/components/reimbursable/Reimbursable.sol");
 
 
@@ -10,9 +10,8 @@ contract('ComponetList', (accounts) => {
   });
   it("Set and Get ", async () => log.catch(async () => {
     const reimbursable = await Reimbursable.deployed();
-    await list.setComponent('reiumbrsable', 'v1', reimbursable.address, { from: accounts[0] });
-
-    const component = await list.getComponent('reiumbrsable', 'v1');
+    await list.setComponent('reiumbrsable', reimbursable.address, { from: accounts[0] });
+    const component = await list.getComponent('reiumbrsable', await reimbursable.version());
     assert.equal(component, reimbursable.address, 'Checking the address is correct');
   }))
 
