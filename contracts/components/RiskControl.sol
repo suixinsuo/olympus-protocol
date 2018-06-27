94e56c1f161ded934cef6db9fcb61bea11f0a0ba
Jerome Chen <jerome@aireach.io>
2018-06-27 11:26:54 +0800

    Fixed RiskControl Path

--- contracts/components/RiskControl.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-import "../interfaces/RiskControlInterface.sol";
-
-contract RiskControl is RiskControlInterface {
-    function hasRisk(address /*_sender*/, address /*_receiver*/, address /*_tokenAddress*/, uint /*_amount*/, uint /*_rate*/)
-        external view returns(bool isRisky) {
-        return false;
-    }
-}
