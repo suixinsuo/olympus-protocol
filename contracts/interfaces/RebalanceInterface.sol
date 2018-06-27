682cfbcd13859b94070aac2e8123880162eb1f11
Gerdinand <gerdinand.hardeman@outlook.com>
2018-06-27 14:05:02 +0800

    fix build

--- contracts/interfaces/RebalanceInterface.sol
+++ contracts/interfaces/RebalanceInterface.sol
@@ -7,6 +7,6 @@ contract RebalanceInterface is ComponentInterface {
     // this should be called until it returns true.
     function recalculateTokensToBuyAfterSale(uint _receivedETHFromSale, uint[] _amountsToBuy) external pure
         returns(uint[] _recalculatedAmountsToBuy);
-    function rebalanceGetTokensToSellAndBuy() external returns
+    function rebalanceGetTokensToSellAndBuy() external view returns
         (address[] _tokensToSell, uint[] _amountsToSell, address[] _tokensToBuy, uint[] _amountsToBuy, address[] _tokensWithPriceIssues);
 }
