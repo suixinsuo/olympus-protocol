c4dab3386760440a1ae38e9190af81926566fc28
Abel Bordonado Lillo <abel.bordonado@gmail.com>
2018-07-04 18:08:29 +0800

    Remove log catch

--- test/marketplace/TestMarketPlace.js
+++ test/marketplace/TestMarketPlace.js
@@ -1,17 +1,15 @@
-const log = require('../utils/log');
+const log = require("../utils/log");
 const MockMarketClient = artifacts.require("MockMarketClient");
 const MarketplaceProvider = artifacts.require("../../contracts/Marketplace.sol");
 
-
-
-contract('Marketplace', (accounts) => {
+contract("Marketplace", accounts => {
   let marketplace;
 
-  before('Run marketplace', async () => {
+  before("Run marketplace", async () => {
     marketplace = await MarketplaceProvider.deployed();
   });
 
-  it("Marketplace returns registered products", async () => log.catch(async () => {
+  it("Marketplace returns registered products", async () => {
     const product1 = await MockMarketClient.new();
     await product1.register(marketplace.address);
     const product2 = await MockMarketClient.new();
@@ -23,25 +21,23 @@ contract('Marketplace', (accounts) => {
 
     const myProducts = await marketplace.getOwnProducts();
 
-    assert.equal(myProducts.length, 2, 'Account 0 has 2 produts');
+    assert.equal(myProducts.length, 2, "Account 0 has 2 produts");
     assert.equal(myProducts[0], product1.address);
     assert.equal(myProducts[1], product2.address);
 
     const allProducts = await marketplace.getAllProducts();
-    assert.equal(allProducts.length, 3, 'All are 3 produts');
-  }));
+    assert.equal(allProducts.length, 3, "All are 3 produts");
+  });
 
-  it("Marketplace avoid duplicated registration", async () => log.catch(async () => {
+  it("Marketplace avoid duplicated registration", async () => {
     const product1 = await MockMarketClient.new();
     const tx1 = await product1.register(marketplace.address);
     assert.equal(parseInt(tx1.receipt.status, 16), 1);
     try {
       await product1.register(marketplace.address);
-      assert(false, 'Must revert');
+      assert(false, "Must revert");
     } catch (e) {
       assert(true);
     }
-
-  }));
-
+  });
 });
