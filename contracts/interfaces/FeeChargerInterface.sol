d50e952e4863e3671c6c9f29ee252a513e227d5e
Jerome Chen <jerome@secondcompany.nl>
2018-07-04 14:44:17 +0800

    Fix accoring to review.

--- contracts/interfaces/FeeChargerInterface.sol
+++ contracts/interfaces/FeeChargerInterface.sol
@@ -7,12 +7,4 @@ contract FeeChargerInterface {
     // TODO: change this to mainnet MOT address before deployment.
     // solhint-disable-next-line
     ERC20Extended public MOT = ERC20Extended(0x41Dee9F481a1d2AA74a3f1d0958C1dB6107c686A);
-
-    // function adjustFeeMode(uint _newMode) external returns (bool);
-    // function adjustFeeAmount(uint _newAmount) external returns (bool);
-    // function adjustFeePercentage(uint _newPercentage) external returns (bool);
-    // function setWalletId(address _newWallet) external returns (bool);
-    // function setMotAddress(address _motAddress) external returns (bool);
-
-    // function calculateFee(uint _amount) external view returns (uint amount);
 }
