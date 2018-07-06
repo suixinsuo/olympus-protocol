db160e87c0b51b5deebc2fe35274c06bb8c939c9
Jerome Chen <jerome@secondcompany.nl>
2018-07-06 18:59:41 +0800

    Added setMotAddress to FeeChargerInterface.

--- contracts/interfaces/FeeChargerInterface.sol
+++ contracts/interfaces/FeeChargerInterface.sol
@@ -8,4 +8,5 @@ contract FeeChargerInterface {
     // solhint-disable-next-line
     ERC20Extended public MOT = ERC20Extended(0x263c618480DBe35C300D8d5EcDA19bbB986AcaeD);
     // kovan MOT: 0x41Dee9F481a1d2AA74a3f1d0958C1dB6107c686A
+    function setMotAddress(address _motAddress) external returns (bool success);
 }
