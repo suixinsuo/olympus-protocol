4dfd5df3e29050854bdfc48220dc2fbd265df8a5
Jerome Chen <jerome@aireach.io>
2018-07-09 17:45:43 +0800

    Component list and tests.

--- contracts/ComponentList.sol
+++ contracts/ComponentList.sol
@@ -2,21 +2,63 @@ pragma solidity 0.4.24;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./interfaces/ComponentInterface.sol";
+import "./interfaces/ComponentListInterface.sol";
+import "./libs/Converter.sol";
 
-contract ComponentList is Ownable{
 
-    mapping(string => mapping(string => address)) components;
+contract ComponentList is ComponentListInterface, Ownable {
 
-    event ComponentUpdated (string _name, string _version, address _componentAddress);
+    mapping(string => mapping(string => address)) private components;
+    mapping(string => string[]) private componentVersions;
 
-    function setComponent(string _name, address _componentAddress) public payable onlyOwner{
+    function setComponent(string _name, address _componentAddress) public onlyOwner returns (bool) {
         ComponentInterface c = ComponentInterface(_componentAddress);
         components[_name][c.version()] = _componentAddress;
+        bool included = false;
+        uint index = 0;
+        for (uint i = 0; i < componentVersions[_name].length; i++) {
+            if (keccak256(abi.encodePacked(componentVersions[_name][i])) == keccak256(abi.encodePacked(c.version()))) {
+                included = true;
+                index = i;
+                break;
+            }
+        }
+        if (included) {
+            remove(_name, index);
+        }
+
+        componentVersions[_name].push(c.version());
+
         emit ComponentUpdated(_name, c.version(), _componentAddress);
+
+        return true;
     }
 
-    function getComponent(string _name, string _version) public view returns (address){
+    function getComponent(string _name, string _version) public view returns (address) {
         return components[_name][_version];
     }
+
+    function getLatestComponent(string _name) public view returns(address) {
+        return components[_name][componentVersions[_name][componentVersions[_name].length - 1]];
+    }
+
+    function getComponentVersions(string _name) public view returns (bytes32[] results) {
+        results = new bytes32[](componentVersions[_name].length);
+        for (uint i = 0; i < componentVersions[_name].length; i++) {
+            results[i] = Converter.stringToBytes32(componentVersions[_name][i]);
+        }
+
+        return results;
+    }
     
+    function remove(string _name, uint _index) private returns(string[]) {
+        string[] storage array = componentVersions[_name];
+        if (_index >= array.length) return;
+
+        for (uint i = _index; i < array.length-1; i++) {
+            array[i] = array[i+1];
+        }
+        array.length--;
+        return array;
+    }
 }
--- test/componentList/ComponentList.js
+++ test/componentList/ComponentList.js
@@ -2,54 +2,100 @@ const log = require("../utils/log");
 const ComponentList = artifacts.require("ComponentList");
 const MockComponent = artifacts.require("MockComponent");
 
+function bytes32ToString(bytes32) {
+  return web3.toAscii(bytes32).replace(/\u0000/g, "");
+}
+
 contract("ComponetList", accounts => {
   let list;
   before("ComponentList Test", async () => {
     list = await ComponentList.new();
   });
-  it("Set and Get ", async () =>
-    log.catch(async () => {
-      const mockComponent = await MockComponent.new("New");
-      await list.setComponent("reiumbrsable", mockComponent.address, { from: accounts[0] });
-      const component = await list.getComponent("reiumbrsable", await mockComponent.version());
-      assert.equal(component, mockComponent.address, "Checking the address is correct");
-    }));
+  it("Set and Get ", async () => {
+    const mockComponent = await MockComponent.new("New");
+    await list.setComponent("reiumbrsable", mockComponent.address, { from: accounts[0] });
+    const component = await list.getComponent("reiumbrsable", await mockComponent.version());
+    assert.equal(component, mockComponent.address, "Checking the address is correct");
+  });
 
-  it("Override component", async () =>
-    log.catch(async () => {
-      const olderMockComponent = await MockComponent.new("Older");
-      const newMockComponent = await MockComponent.new("New");
-      // set olderMockComponent's address into componentList contract.
-      await list.setComponent("reiumbrsable", olderMockComponent.address, { from: accounts[0] });
-      let component = await list.getComponent("reiumbrsable", await olderMockComponent.version());
-      assert.equal(component, olderMockComponent.address, "Checking the address is correct");
-      // Override the olderMockComponent's address
-      await list.setComponent("reiumbrsable", newMockComponent.address, { from: accounts[0] });
-      component = await list.getComponent("reiumbrsable", await newMockComponent.version());
-      assert.equal(
-        component,
-        newMockComponent.address,
-        "Override the component successfully: it is the same with newMockComponent"
-      );
-      assert.notEqual(
-        component,
-        olderMockComponent.address,
-        "Override the component successfully: olderMockComponent is different with the address from component list"
-      );
-    }));
+  it("Override component", async () => {
+    const olderMockComponent = await MockComponent.new("Older");
+    const newMockComponent = await MockComponent.new("New");
+    // set olderMockComponent's address into componentList contract.
+    await list.setComponent("reiumbrsable", olderMockComponent.address, { from: accounts[0] });
+    let component = await list.getComponent("reiumbrsable", await olderMockComponent.version());
+    assert.equal(component, olderMockComponent.address, "Checking the address is correct");
+    // Override the olderMockComponent's address
+    await list.setComponent("reiumbrsable", newMockComponent.address, { from: accounts[0] });
+    component = await list.getComponent("reiumbrsable", await newMockComponent.version());
+    assert.equal(
+      component,
+      newMockComponent.address,
+      "Override the component successfully: it is the same with newMockComponent"
+    );
+    assert.notEqual(
+      component,
+      olderMockComponent.address,
+      "Override the component successfully: olderMockComponent is different with the address from component list"
+    );
+  });
 
-  it("update version", async () =>
-    log.catch(async () => {
-      const mockComponentV1 = await MockComponent.new("V1");
-      const mockComponentV2 = await MockComponent.new("V2");
-      // set olderMockComponent's address into componentList contract.
-      await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
-      await list.setComponent("reiumbrsable", mockComponentV2.address, { from: accounts[0] });
-      //mockComponentV1
-      let component = await list.getComponent("reiumbrsable", await mockComponentV1.version());
-      assert.equal(component, mockComponentV1.address, "mockComponentV1's address is correct");
-      //mockComponentV2
-      component = await list.getComponent("reiumbrsable", await mockComponentV2.version());
-      assert.equal(component, mockComponentV2.address, "mockComponentV2's address is correct");
-    }));
+  it("update version", async () => {
+    const mockComponentV1 = await MockComponent.new("V1");
+    const mockComponentV2 = await MockComponent.new("V2");
+    // set olderMockComponent's address into componentList contract.
+    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
+    await list.setComponent("reiumbrsable", mockComponentV2.address, { from: accounts[0] });
+    //mockComponentV1
+    let component = await list.getComponent("reiumbrsable", await mockComponentV1.version());
+    assert.equal(component, mockComponentV1.address, "mockComponentV1's address is correct");
+    //mockComponentV2
+    component = await list.getComponent("reiumbrsable", await mockComponentV2.version());
+    assert.equal(component, mockComponentV2.address, "mockComponentV2's address is correct");
+  });
+
+  it("permission check", async () => {
+    const mockComponentV1 = await MockComponent.new("V1");
+    const mockComponentV2 = await MockComponent.new("V2");
+    // set olderMockComponent's address into componentList contract.
+    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
+
+    try {
+      await list.setComponent("reiumbrsable", mockComponentV2.address, { from: accounts[1] });
+      assert(false, "should revert");
+    } catch (e) {
+      if (e.message.includes("revert")) {
+        assert(true, "Non-owner can't call it.");
+      } else {
+        assert(false, "should revert");
+        throw e;
+      }
+    }
+  });
+
+  it("get latest version", async () => {
+    const mockComponentV1 = await MockComponent.new("V1");
+    const mockComponentV2 = await MockComponent.new("V2");
+
+    list = await ComponentList.new();
+
+    // set olderMockComponent's address into componentList contract.
+    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
+    let component = await list.getLatestComponent("reiumbrsable");
+    let versions = (await list.getComponentVersions("reiumbrsable")).map(bytes32ToString);
+    assert.deepEqual(versions, ["V1"]);
+    assert.equal(MockComponent(component).version(), await mockComponentV1.version(), "version 1 should be the same");
+
+    versions = (await list.getComponentVersions("reiumbrsable")).map(bytes32ToString);
+    assert.deepEqual(versions, ["V1", "V2"]);
+    await list.setComponent("reiumbrsable", mockComponentV2.address, { from: accounts[0] });
+    component = await list.getLatestComponent("reiumbrsable");
+    assert.equal(MockComponent(component).version(), await mockComponentV2.version(), "version 2 should be the same");
+
+    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
+    component = await list.getLatestComponent("reiumbrsable");
+    assert.equal(MockComponent(component).version(), await mockComponentV1.version(), "takes the lastest version");
+    versions = (await list.getComponentVersions("reiumbrsable")).map(bytes32ToString);
+    assert.deepEqual(versions, ["V2", "V1"]);
+  });
 });
