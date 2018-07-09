9409cc8e7a0c06e3d3ac4e714645b7e0224ccf57
Jerome Chen <jerome@secondcompany.nl>
2018-07-09 18:23:51 +0800

    Fixed review points.

--- test/componentList/ComponentList.js
+++ test/componentList/ComponentList.js
@@ -13,8 +13,8 @@ contract("ComponetList", accounts => {
   });
   it("Set and Get ", async () => {
     const mockComponent = await MockComponent.new("New");
-    await list.setComponent("reiumbrsable", mockComponent.address, { from: accounts[0] });
-    const component = await list.getComponent("reiumbrsable", await mockComponent.version());
+    await list.setComponent("reimbursable", mockComponent.address, { from: accounts[0] });
+    const component = await list.getComponent("reimbursable", await mockComponent.version());
     assert.equal(component, mockComponent.address, "Checking the address is correct");
   });
 
@@ -22,12 +22,12 @@ contract("ComponetList", accounts => {
     const olderMockComponent = await MockComponent.new("Older");
     const newMockComponent = await MockComponent.new("New");
     // set olderMockComponent's address into componentList contract.
-    await list.setComponent("reiumbrsable", olderMockComponent.address, { from: accounts[0] });
-    let component = await list.getComponent("reiumbrsable", await olderMockComponent.version());
+    await list.setComponent("reimbursable", olderMockComponent.address, { from: accounts[0] });
+    let component = await list.getComponent("reimbursable", await olderMockComponent.version());
     assert.equal(component, olderMockComponent.address, "Checking the address is correct");
     // Override the olderMockComponent's address
-    await list.setComponent("reiumbrsable", newMockComponent.address, { from: accounts[0] });
-    component = await list.getComponent("reiumbrsable", await newMockComponent.version());
+    await list.setComponent("reimbursable", newMockComponent.address, { from: accounts[0] });
+    component = await list.getComponent("reimbursable", await newMockComponent.version());
     assert.equal(
       component,
       newMockComponent.address,
@@ -44,13 +44,13 @@ contract("ComponetList", accounts => {
     const mockComponentV1 = await MockComponent.new("V1");
     const mockComponentV2 = await MockComponent.new("V2");
     // set olderMockComponent's address into componentList contract.
-    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
-    await list.setComponent("reiumbrsable", mockComponentV2.address, { from: accounts[0] });
+    await list.setComponent("reimbursable", mockComponentV1.address, { from: accounts[0] });
+    await list.setComponent("reimbursable", mockComponentV2.address, { from: accounts[0] });
     //mockComponentV1
-    let component = await list.getComponent("reiumbrsable", await mockComponentV1.version());
+    let component = await list.getComponent("reimbursable", await mockComponentV1.version());
     assert.equal(component, mockComponentV1.address, "mockComponentV1's address is correct");
     //mockComponentV2
-    component = await list.getComponent("reiumbrsable", await mockComponentV2.version());
+    component = await list.getComponent("reimbursable", await mockComponentV2.version());
     assert.equal(component, mockComponentV2.address, "mockComponentV2's address is correct");
   });
 
@@ -58,10 +58,10 @@ contract("ComponetList", accounts => {
     const mockComponentV1 = await MockComponent.new("V1");
     const mockComponentV2 = await MockComponent.new("V2");
     // set olderMockComponent's address into componentList contract.
-    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
+    await list.setComponent("reimbursable", mockComponentV1.address, { from: accounts[0] });
 
     try {
-      await list.setComponent("reiumbrsable", mockComponentV2.address, { from: accounts[1] });
+      await list.setComponent("reimbursable", mockComponentV2.address, { from: accounts[1] });
       assert(false, "should revert");
     } catch (e) {
       if (e.message.includes("revert")) {
@@ -80,9 +80,9 @@ contract("ComponetList", accounts => {
     list = await ComponentList.new();
 
     // set olderMockComponent's address into componentList contract.
-    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
-    let component = await list.getLatestComponent("reiumbrsable");
-    let versions = (await list.getComponentVersions("reiumbrsable")).map(bytes32ToString);
+    await list.setComponent("reimbursable", mockComponentV1.address, { from: accounts[0] });
+    let component = await list.getLatestComponent("reimbursable");
+    let versions = (await list.getComponentVersions("reimbursable")).map(bytes32ToString);
     assert.deepEqual(versions, ["V1"]);
     assert.equal(
       await MockComponent.at(component).version(),
@@ -90,24 +90,24 @@ contract("ComponetList", accounts => {
       "version 1 should be the same"
     );
 
-    await list.setComponent("reiumbrsable", mockComponentV2.address, { from: accounts[0] });
-    versions = (await list.getComponentVersions("reiumbrsable")).map(bytes32ToString);
+    await list.setComponent("reimbursable", mockComponentV2.address, { from: accounts[0] });
+    versions = (await list.getComponentVersions("reimbursable")).map(bytes32ToString);
     assert.deepEqual(versions, ["V1", "V2"]);
-    component = await list.getLatestComponent("reiumbrsable");
+    component = await list.getLatestComponent("reimbursable");
     assert.equal(
       await MockComponent.at(component).version(),
       await mockComponentV2.version(),
       "version 2 should be the same"
     );
 
-    await list.setComponent("reiumbrsable", mockComponentV1.address, { from: accounts[0] });
-    component = await list.getLatestComponent("reiumbrsable");
+    await list.setComponent("reimbursable", mockComponentV1.address, { from: accounts[0] });
+    component = await list.getLatestComponent("reimbursable");
     assert.equal(
       await MockComponent.at(component).version(),
       await mockComponentV1.version(),
       "should take the lastest version"
     );
-    versions = (await list.getComponentVersions("reiumbrsable")).map(bytes32ToString);
+    versions = (await list.getComponentVersions("reimbursable")).map(bytes32ToString);
     assert.deepEqual(versions, ["V2", "V1"]);
   });
 });
