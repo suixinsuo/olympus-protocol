9b9a7068fe8c25d6d83c249277dd67633f57c490
Jerome Chen <jerome@secondcompany.nl>
2018-07-23 20:04:14 +0800

    MVP Fund

---
+++ contracts/olympusProtocols/OlympusBaseFund.sol
@@ -0,0 +1,290 @@
+pragma solidity 0.4.24;
+
+import "../Derivative.sol";
+import "../interfaces/FundInterface.sol";
+import "../interfaces/implementations/OlympusExchangeInterface.sol";
+import "../interfaces/WithdrawInterface.sol";
+import "../interfaces/MarketplaceInterface.sol";
+import "../interfaces/ChargeableInterface.sol";
+import "../interfaces/StepInterface.sol";
+import "../libs/ERC20NoReturn.sol";
+
+
+contract OlympusBaseFund is FundInterface, Derivative {
+    using SafeMath for uint256;
+    
+    uint public constant DENOMINATOR = 10000;
+    uint public constant INITIAL_VALUE =  10**18; // 1 ETH
+
+    event TokenUpdated(address _token, uint amount);
+    event FundStatusChanged(DerivativeStatus status);
+
+    mapping(address => uint) public investors;
+    mapping(address => uint) public amounts;
+    mapping(address => bool) public activeTokens;
+
+   uint public accumulatedFee = 0;
+
+    constructor(
+      string _name,
+      string _symbol,
+      string _description,
+      string _category,
+      uint _decimals
+     ) public {
+        name = _name;
+        symbol = _symbol;
+        category = _category;
+        description = _description;
+        version = "1.0";
+        decimals = _decimals;
+        status = DerivativeStatus.New;
+        fundType = DerivativeType.Fund;
+    }
+
+    // Call after you have updated the MARKET provider, not required after initialize
+    function registerInNewMarketplace() external onlyOwner returns(bool) {
+        require(MarketplaceInterface(getComponentByName(MARKET)).registerProduct());
+        return true;
+    }
+
+    // ----------------------------- CONFIG -----------------------------
+    // One time call
+    function initialize(address _componentList) external onlyOwner payable {
+        require(_componentList != 0x0);
+        require(status == DerivativeStatus.New);
+        require(msg.value > 0); // Require some balance for internal opeations as reimbursable
+
+        super.initialize(_componentList);
+        bytes32[4] memory names = [MARKET, EXCHANGE, WITHDRAW, STEP];
+        bytes32[] memory nameParameters = new bytes32[](names.length);
+
+        for (uint i = 0; i < names.length; i++) {
+            nameParameters[i] = names[i];
+        }
+        setComponents(
+            nameParameters,
+            componentList.getLatestComponents(nameParameters)
+        );
+
+        // approve component for charging fees.
+        approveComponents();
+
+        MarketplaceInterface(getComponentByName(MARKET)).registerProduct();
+        StepInterface(getComponentByName(STEP)).setMaxCalls(WITHDRAW, 10);
+
+        status = DerivativeStatus.Active;
+        emit FundStatusChanged(status);
+
+        accumulatedFee += msg.value;
+    }
+
+    function getTokens() external view returns(address[], uint[]) {
+        uint[] memory _amounts = new uint[](tokens.length);
+        for (uint i = 0; i < tokens.length; i++) {
+            _amounts[i] = amounts[tokens[i]];
+        }
+        return (tokens, _amounts);
+    }
+
+    // ----------------------------- FUND INTERFACE -----------------------------
+    function buyTokens(bytes32 _exchangeId, ERC20Extended[] _tokens, uint[] _amounts, uint[] _minimumRates)
+         public onlyOwner returns(bool) {
+
+         // Check we have the ethAmount required
+        uint totalEthRequired = 0;
+        for (uint i = 0; i < _amounts.length; i++) {
+          require(!hasRisk(address(this), getComponentByName(EXCHANGE), ETH, _amounts[i], _minimumRates[i]));
+          totalEthRequired += _amounts[i];
+        }
+        require(getETHBalance() >= totalEthRequired);
+
+        require(OlympusExchangeInterface(getComponentByName(EXCHANGE))
+          .buyTokens.value(totalEthRequired)(_tokens, _amounts, _minimumRates, address(this), _exchangeId, 0x0)
+        );
+        updateTokens(_tokens);
+        return true;
+
+    }
+
+    function sellTokens(bytes32 _exchangeId, ERC20Extended[] _tokens, uint[] _amounts, uint[]  _rates)
+      public onlyOwner returns (bool) {
+
+        OlympusExchangeInterface exchange = OlympusExchangeInterface(getComponentByName(EXCHANGE));
+
+        for (uint i = 0; i < tokens.length; i++) {
+            require(!hasRisk(address(this), exchange, address(_tokens[i]), _amounts[i], _rates[i]));
+            ERC20NoReturn(_tokens[i]).approve(exchange, 0);
+            ERC20NoReturn(_tokens[i]).approve(exchange, _amounts[i]);
+        }
+
+        require(exchange.sellTokens(_tokens, _amounts, _rates, address(this), _exchangeId, 0x0));
+        updateTokens(_tokens);
+        return true;
+    }
+     // ----------------------------- DERIVATIVE -----------------------------
+
+    function invest() public
+        payable
+        whenNotPaused
+      returns(bool) {
+        require(status == DerivativeStatus.Active, "The Fund is not active");
+        require(msg.value >= 10**15, "Minimum value to invest is 0.001 ETH");
+         // Current value is already added in the balance, reduce it
+        uint _sharePrice;
+
+        if (totalSupply_ > 0) {
+            _sharePrice = getPrice() - ((msg.value * 10 ** decimals) / totalSupply_);
+         } else {
+            _sharePrice = INITIAL_VALUE;
+        }
+
+        uint _investorShare = (((msg.value) * DENOMINATOR) / _sharePrice) * 10 ** decimals;
+        _investorShare = _investorShare / DENOMINATOR;
+
+        balances[msg.sender] += _investorShare;
+        totalSupply_ += _investorShare;
+
+        return true;
+    }
+
+    function changeStatus(DerivativeStatus _status) public onlyOwner returns(bool) {
+        require(_status != DerivativeStatus.New && status != DerivativeStatus.New);
+        require(status != DerivativeStatus.Closed && _status != DerivativeStatus.Closed);
+        status = _status;
+        emit FundStatusChanged(status);
+        return true;
+    }
+
+    function close() public onlyOwner returns(bool success) {
+        require(status != DerivativeStatus.New);
+        getETHFromTokens(DENOMINATOR); // 100% all the tokens
+        status = DerivativeStatus.Closed;
+        emit FundStatusChanged(status);
+        return true;
+    }
+
+    function getPrice() public view returns(uint) {
+        if (totalSupply_ == 0) {
+            return INITIAL_VALUE;
+        }
+
+        // Total Value in ETH among its tokens + ETH new added value
+        return (
+          ((getAssetsValue() + getETHBalance()) * 10 ** decimals) / (totalSupply_),
+        );
+    }
+
+    function getETHBalance() public view returns(uint) {
+        return address(this).balance - accumulatedFee;
+    }
+
+    function getAssetsValue() public view returns (uint) {
+        // TODO cast to OlympusExchangeInterface
+        OlympusExchangeInterface exchangeProvider = OlympusExchangeInterface(getComponentByName(EXCHANGE));
+        uint _totalTokensValue = 0;
+        // Iterator
+        uint _expectedRate;
+        uint _balance;
+
+        for (uint16 i = 0; i < tokens.length; i++) {
+            _balance = ERC20(tokens[i]).balanceOf(address(this));
+            if (_balance == 0) { continue; }
+
+            (_expectedRate, ) = exchangeProvider.getPrice(ETH, ERC20Extended(tokens[i]), _balance, 0x0);
+
+            if (_expectedRate == 0) { continue; }
+            _totalTokensValue += (_balance * 10**18) / _expectedRate;
+
+        }
+        return _totalTokensValue;
+    }
+
+    // ----------------------------- FEES  -----------------------------
+    // Owner can send ETH to the Index, to perform some task, this eth belongs to him
+    // solhint-disable-next-line
+    function addOwnerBalance() external payable onlyOwner {
+        accumulatedFee += msg.value;
+    }
+
+    // solhint-disable-next-line
+    function withdrawFee(uint amount) external onlyOwner whenNotPaused returns(bool) {
+        require(accumulatedFee >= amount);
+        accumulatedFee -= amount;
+        msg.sender.transfer(amount);
+        return true;
+    }
+
+   // ----------------------------- WITHDRAW -----------------------------
+   // solhint-disable-next-line
+   function withdraw()
+        external
+        whenNotPaused
+        returns(bool)
+    {
+        //todo: use the simple withdraw
+    }
+
+    // solhint-disable-next-line
+    function tokensWithAmount() public view returns( ERC20Extended[] memory) {
+        // First check the length
+        uint length = 0;
+        for (uint i = 0; i < tokens.length; i++) {
+            if (amounts[tokens[i]] > 0) {length++;}
+        }
+
+        ERC20Extended[] memory _tokensWithAmount = new ERC20Extended[](length);
+        // Then create they array
+        uint index = 0;
+        for (uint j = 0; j < tokens.length; j++) {
+            if (amounts[tokens[j]] > 0) {
+                _tokensWithAmount[index] = ERC20Extended(tokens[j]);
+                index++;
+            }
+        }
+        return _tokensWithAmount;
+    }
+
+    // solhint-disable-next-line
+    function getETHFromTokens(uint _tokenPercentage) public onlyOwner {
+        ERC20Extended[] memory _tokensToSell = tokensWithAmount();
+        uint[] memory _amounts = new uint[](_tokensToSell.length);
+        uint[] memory _sellRates = new uint[](_tokensToSell.length);
+        OlympusExchangeInterface exchange = OlympusExchangeInterface(getComponentByName(EXCHANGE));
+
+        for (uint i = 0; i < _tokensToSell.length; i++) {
+            _amounts[i] = (_tokenPercentage * _tokensToSell[i].balanceOf(address(this))) / DENOMINATOR;
+            (, _sellRates[i] ) = exchange.getPrice(_tokensToSell[i], ETH, _amounts[i], 0x0);
+            require(!hasRisk(address(this), exchange, address(_tokensToSell[i]), _amounts[i], _sellRates[i]));
+            ERC20NoReturn(_tokensToSell[i]).approve(exchange, 0);
+            ERC20NoReturn(_tokensToSell[i]).approve(exchange, _amounts[i]);
+        }
+
+        require(exchange.sellTokens(_tokensToSell, _amounts, _sellRates, address(this), 0x0, 0x0));
+        updateTokens(_tokensToSell);
+    }
+
+    function approveComponents() private {
+        approveComponent(EXCHANGE);
+        approveComponent(WITHDRAW);
+        approveComponent(RISK);
+        approveComponent(WHITELIST);
+        approveComponent(FEE);
+        approveComponent(REIMBURSABLE);
+    }
+
+    function updateTokens(ERC20Extended[] _updatedTokens) private returns(bool success) {
+        ERC20 _tokenAddress;
+        for (uint i = 0; i < _updatedTokens.length; i++) {
+            _tokenAddress = _updatedTokens[i];
+            amounts[_tokenAddress] = _tokenAddress.balanceOf(this);
+            if (amounts[_tokenAddress] > 0 && !activeTokens[_tokenAddress]) {
+                tokens.push(_tokenAddress);
+                activeTokens[_tokenAddress] = true;
+                continue;
+            }
+            emit TokenUpdated(_tokenAddress, amounts[_tokenAddress]);
+        }
+        return true;
+    }
+}
