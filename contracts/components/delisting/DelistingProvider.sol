96b490dfd3a1c2d55adf1b6cd0bfe3a425783bc7
OrangeM21 <orangemtony@gmail.com>
2018-08-10 15:06:44 +0800

    delete delisting provider

--- contracts/components/delisting/DelistingProvider.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.24;
-
-import "../../interfaces/DelistingInterface.sol";
-
-
-contract Delisting is DelistingInterface {
-    
-}
--- contracts/interfaces/DelistingInterface.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.24;
-
-import "./ComponentInterface.sol";
-
-
-contract DelistingInterface is ComponentInterface {
-    
-}
