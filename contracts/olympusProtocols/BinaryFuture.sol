5243a8546e9bf59575261da184a15cceacf7b55b
Jerome Chen <jerome@aireach.io>
2018-12-06 10:33:08 +0800

    Fixed invest 0

--- contracts/olympusProtocols/BinaryFuture.sol
+++ contracts/olympusProtocols/BinaryFuture.sol
@@ -155,6 +155,7 @@ contract BinaryFuture is BaseDerivative, BinaryFutureInterface {
 
         require(status == DerivativeStatus.Active,"3");
         require(_period == _currentPeriod, "5");
+        require(msg.value > 0, "13");
         require(
             getSupplyByPeriod(LONG, _period).add(getSupplyByPeriod(SHORT, _period)) < MAX_INVESTORS,
             "11"
