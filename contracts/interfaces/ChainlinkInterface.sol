dbeea66f602a1e6cb72f5ef69e29c93e1865b0ee
OrangeM21 <orangemtony@gmail.com>
2018-12-14 15:49:01 +0800

    fixed

--- contracts/interfaces/ChainlinkInterface.sol
+++ contracts/interfaces/ChainlinkInterface.sol
@@ -2,6 +2,8 @@ pragma solidity 0.4.24;
 import "./ComponentInterface.sol";
 contract ChainlinkInterface is ComponentInterface {
     function requestEthereumPrice(string _jobId, string _currency) public;
+    function requestEthereumChange(string _jobId, string _currency) public;
+    function requestEthereumLastMarket(string _jobId, string _currency) public;
     function getCurrentPrice() public view returns(uint256);
     function getLastUpdateTime() public view returns(uint256);
 
