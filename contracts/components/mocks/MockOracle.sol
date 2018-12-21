0c70c2f31818fbeba8069ad7ef5c7f3d95946521
OrangeM21 <orangemtony@gmail.com>
2018-12-21 20:41:10 +0800

    fix

--- contracts/components/mocks/MockOracle.sol
+++ contracts/components/mocks/MockOracle.sol
@@ -3,8 +3,11 @@ import "../Oracle/ChainlinkOracle.sol";
 
 contract MockOracle is ChainlinkOracle {
     function setMockTargetPrice(uint _targetPrice) public  {
-        currentPrice = (ETH_PRECISION).mul(USD_PRECISION).div(_targetPrice);
+        currentPrice = _targetPrice;
         lastUpdateTime = now;
+    }
+    function getCurrentPrice(uint _currencyUnitType) public view returns(uint256){
+        return currentPrice;
     }
     function setMockLastuUdateTime(uint _targetTime) public  {
         lastUpdateTime = _targetTime;
