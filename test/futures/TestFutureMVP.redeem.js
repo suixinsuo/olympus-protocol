00f5d8dc0570b5d5ec016d709b78486fc52f6392
vellengs <vellengs@qq.com>
2019-01-18 20:21:49 +0800

    remove comments

--- test/futures/TestFutureMVP.redeem.js
+++ test/futures/TestFutureMVP.redeem.js
@@ -62,7 +62,6 @@ contract("Test Future MVP Redeem", accounts => {
 
     await calc.assertReverts(async () => {
       await utils.estimateValue(future, FutureDirection.Short, 1, 10 ** 18);
-      // await utils.estimateValue(future,FutureDirection.Short, 1, 10 ** 18);
     }, "Shall revert");
 
     await future.initialize(providers.componentList.address, futureData.clearInterval, {
@@ -73,38 +72,28 @@ contract("Test Future MVP Redeem", accounts => {
       assert.equal(0, +value, 'should be return 0');
     });
 
-    // set mock price 1 ETH.
     await mockOracle.setMockTargetPrice(10 ** 18);
-    // account 1 invest( -1, 2);
     await utils.safeInvest(future, FutureDirection.Long, amountsOfShares, accounts[1],
       investmentMargin);
-    // account 2 invest( 1, 2).
     await utils.safeInvest(future, FutureDirection.Short, amountsOfShares, accounts[2],
       investmentMargin);
-    // estimateValue(1, 1, 100000000000000000), should be return 0.2ETH, 
     await utils.estimateValue(future, FutureDirection.Long, 1, 10 ** 18).then((value) => {
       assert.equal(0.2 * 10 ** 18, +value, 'should be return 0.2ETH');
     });
-    // estimateValue(-1, 1, 100000000000000000), should be return 0.2ETH, 
     await utils.estimateValue(future, FutureDirection.Short, 1, 10 ** 18).then((value) => {
       assert.equal(0.2 * 10 ** 18, +value, 'should be return 0.2ETH');
     });
 
     await mockOracle.setMockTargetPrice(0.91 * 10 ** 18);
-    // estimateValue(1, 1, 100000000000000000), should be return 0.2ETH, 
     await utils.estimateValue(future, FutureDirection.Short, 1, 10 ** 18).then((value) => {
       assert.equal(0.2 * 10 ** 18, +value, 'should be return 0.2ETH');
     });
-    // estimateValue(1, 1, 91000000000000000), should be return 0.2ETH, 
     await utils.estimateValue(future, FutureDirection.Short, 1, 0.91 * 10 ** 18).then((value) => {
-      // console.log('should be return 0.38ETH', +value);
       assert.equal(0.38 * 10 ** 18, +value, 'should be return 0.38ETH');
     });
     await utils.estimateValue(future, FutureDirection.Short, 1, 0.90 * 10 ** 18).then((value) => {
-      // console.log('should be return 0.38ETH', +value);
       assert.equal(0.4 * 10 ** 18, +value, 'should be return 0.4ETH');
     });
-    // estimateValue(-1, 1, 100000000000000000), should be return 0.2ETH, 
     await utils.estimateValue(future, FutureDirection.Long, 1, 10 ** 18).then((value) => {
       assert.equal(0.2 * 10 ** 18, +value, 'should be return 0.2ETH');
     });
@@ -118,7 +107,7 @@ contract("Test Future MVP Redeem", accounts => {
     });
 
     await utils.safeCheckPosition(future).then((tx) => {
-      // TODO account 2 should be got 0.4ETH;
+      // Might here check the return event value.
       assert.ok(tx, 'Can redeem until next check position');
     });
 
