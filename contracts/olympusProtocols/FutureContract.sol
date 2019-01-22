76475bd42e3f5678f6917e358f7988f1f4f436b3
vellengs <vellengs@qq.com>
2019-01-22 15:41:34 +0800

    fix calculate issue

--- contracts/olympusProtocols/FutureContract.sol
+++ contracts/olympusProtocols/FutureContract.sol
@@ -357,7 +357,7 @@ contract FutureContract is BaseDerivative, FutureInterfaceV1 {
         }
         
         reimburse();
-        return true;
+        return completed;
     }
  
     /// --------------------------------- END CHECK POSITION ---------------------------------
